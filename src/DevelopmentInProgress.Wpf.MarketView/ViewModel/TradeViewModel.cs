4fd4f49f74af73ea8cfcdc87a2a6e4b13ed32f9b
grantcolley <grantcolley@yahoo.com>
2018-03-14 09:04:46 +0000

    dev in progress
    dev in progress

--- src/DevelopmentInProgress.Wpf.MarketView/ViewModel/TradeViewModel.cs
+++ src/DevelopmentInProgress.Wpf.MarketView/ViewModel/TradeViewModel.cs
@@ -7,11 +7,25 @@ namespace DevelopmentInProgress.Wpf.MarketView.ViewModel
     public class TradeViewModel : BaseViewModel
     {
         private bool disposed;
+        private bool isLoading;
 
         public TradeViewModel(Account account, IExchangeService exchangeService, Action<Exception> exception)
             : base(exchangeService)
         {
+            IsLoading = true;
+        }
 
+        public bool IsLoading
+        {
+            get { return isLoading; }
+            set
+            {
+                if (isLoading != value)
+                {
+                    isLoading = value;
+                    OnPropertyChanged("IsLoading");
+                }
+            }
         }
 
         public override void Dispose(bool disposing)
