832908fee55289821340fa8500afd6f1e4435648
grantcolley <grantcolley@yahoo.com>
2018-03-17 20:33:49 +0000

    Add SelectedSymbol to preferences
    Add SelectedSymbol to preferences

--- src/DevelopmentInProgress.Wpf.MarketView/Personalise/Preferences.cs
+++ src/DevelopmentInProgress.Wpf.MarketView/Personalise/Preferences.cs
@@ -5,6 +5,7 @@ namespace DevelopmentInProgress.Wpf.MarketView.Personalise
     public class Preferences
     {
         public bool ShowFavourites { get; set; }
+        public string SelectedSymbol { get; set; }
         public List<string> FavouriteSymbols { get; set; }
     }
 }
