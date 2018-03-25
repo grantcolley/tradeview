6a2511e5eea706cc369beb84a47b87aba2b6b555
grantcolley <grantcolley@yahoo.com>
2018-03-25 18:37:30 +0100

    Update assembly info for wpf controls
    Update assembly info for wpf controls

--- src/DevelopmentInProgress.Wpf.Controls/Properties/AssemblyInfo.cs
+++ src/DevelopmentInProgress.Wpf.Controls/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Development In Progress Ltd")]
 [assembly: AssemblyProduct("DevelopmentInProgress.Wpf.Controls")]
-[assembly: AssemblyCopyright("Copyright © Development In Progress Ltd 2015")]
+[assembly: AssemblyCopyright("Copyright © Development In Progress Ltd 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.1.0.0")]
-[assembly: AssemblyFileVersion("2.1.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
