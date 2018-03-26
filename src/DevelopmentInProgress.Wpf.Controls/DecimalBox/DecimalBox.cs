58363ff4052bf50e1bcd9838712153b38f270f07
grantcolley <grantcolley@yahoo.com>
2018-03-26 12:02:11 +0100

    Add simple DecimalBox control.
    Add simple DecimalBox control.

---
+++ src/DevelopmentInProgress.Wpf.Controls/DecimalBox/DecimalBox.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace DevelopmentInProgress.Wpf.Controls.DecimalBox
+{
+    partial class DecimalBox
+    {
+        private void OnTextChanged(object sender, TextChangedEventArgs e)
+        {
+            var txt = sender as TextBox;
+            if(txt == null)
+            {
+                return;
+            }
+
+            if(string.IsNullOrWhiteSpace(txt.Text))
+            {
+                return;
+            }
+
+            decimal val;
+            if(Decimal.TryParse(txt.Text, out val))
+            {
+                if (val < 0)
+                {
+                    txt.Text = (val * -1).ToString();
+                }
+            }
+        }
+
+        private void OnClick(object sender, RoutedEventArgs e)
+        {
+            var btn = sender as Button;
+            if(btn == null)
+            {
+                return;
+            }
+
+            var txt = btn.Tag as TextBox;
+            if(txt == null)
+            {
+                return;
+            }
+
+            decimal val;
+            if (string.IsNullOrWhiteSpace(txt.Text))
+            {
+                val = 0;
+            }
+            else
+            {
+                Decimal.TryParse(txt.Text, out val);
+            }
+
+            if(btn.Name.Equals("btnDown"))
+            {
+                if (val > 0)
+                {
+                    txt.Text = (--val).ToString();
+                }
+            }
+            else
+            {
+                txt.Text = (++val).ToString();
+            }
+        }
+    }
+}
---
+++ src/DevelopmentInProgress.Wpf.Controls/DecimalBox/XamlDecimalBox.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace DevelopmentInProgress.Wpf.Controls.DecimalBox
+{
+    public class XamlDecimalBox : Control
+    {
+        private static readonly DependencyProperty ValueProperty;
+
+        static XamlDecimalBox()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(XamlDecimalBox), new FrameworkPropertyMetadata(typeof(XamlDecimalBox)));
+
+            ValueProperty = DependencyProperty.Register("Value", typeof(decimal), typeof(XamlDecimalBox), new PropertyMetadata(0m));
+        }
+
+        public decimal Value
+        {
+            get { return (decimal)GetValue(ValueProperty); }
+            set { SetValue(ValueProperty, value); }
+        }
+    }
+}
