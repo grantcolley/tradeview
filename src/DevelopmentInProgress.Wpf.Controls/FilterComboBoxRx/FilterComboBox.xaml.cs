66f5804fa2b389b94658fac45c66819f62e2b86e
grantcolley <grantcolley@yahoo.com>
2018-03-28 20:16:58 +0100

    Add filter box - wip
    Add filter box - wip

---
+++ src/DevelopmentInProgress.Wpf.Controls/FilterComboBoxRx/FilterComboBox.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace DevelopmentInProgress.Wpf.Controls.FilterComboBoxRx
+{
+    /// <summary>
+    /// Interaction logic for FilterComboBox.xaml
+    /// </summary>
+    public partial class FilterComboBox : UserControl
+    {
+        private static readonly DependencyProperty FilterFieldNameProperty;
+        private static readonly DependencyProperty ItemsSourceProperty;
+
+        static FilterComboBox()
+        {
+            FilterFieldNameProperty = DependencyProperty.Register("FilterFieldName", typeof(string), typeof(FilterComboBox));
+            ItemsSourceProperty = DependencyProperty.Register("ItemsSource", typeof(IEnumerable), typeof(FilterComboBox));
+        }
+
+        public FilterComboBox()
+        {
+            InitializeComponent();            
+        }
+
+
+    }
+}
