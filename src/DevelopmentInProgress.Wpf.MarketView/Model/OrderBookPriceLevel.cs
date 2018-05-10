e66cf94618684f9c809548b48ae586d67db31411
grantcolley <grantcolley@yahoo.com>
2018-05-10 17:43:50 +0100

    Remove obsolete classes and methods
    Remove obsolete classes and methods

--- src/DevelopmentInProgress.Wpf.MarketView/Model/OrderBookPriceLevel.cs
+++ src/DevelopmentInProgress.Wpf.MarketView/Model/OrderBookPriceLevel.cs
@@ -32,42 +32,5 @@ namespace DevelopmentInProgress.Wpf.MarketView.Model
                 }
             }
         }
-
-        public static IComparer<OrderBookPriceLevel> SortByPriceAsc()
-        {
-            return new ComparePriceAsc();
-        }
-
-        public static IComparer<OrderBookPriceLevel> CompareByPriceDesc()
-        {
-            return new ComparePriceAsc();
-        }
-
-        private class ComparePriceAsc : IComparer<OrderBookPriceLevel>
-        {
-            public int Compare(OrderBookPriceLevel x, OrderBookPriceLevel y)
-            {
-                return decimal.Compare(x.Price, y.Price);
-            }
-        }
-
-        public class ComparePriceDesc : IComparer<OrderBookPriceLevel>
-        {
-            public int Compare(OrderBookPriceLevel x, OrderBookPriceLevel y)
-            {
-                if (y.Price < x.Price)
-                {
-                    return -1;
-                }
-                else if (y.Price == x.Price)
-                {
-                    return 0;
-                }
-                else
-                {
-                    return 1;
-                }
-            }
-        }
     }
 }
