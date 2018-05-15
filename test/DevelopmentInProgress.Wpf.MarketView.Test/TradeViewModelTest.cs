4939d2974019f50162f38efadf84a2c5885c7665
grantcolley <grantcolley@yahoo.com>
2018-05-15 13:46:04 +0100

    Add TradeViewModelTest with stubs
    Add TradeViewModelTest with stubs

---
+++ test/DevelopmentInProgress.Wpf.MarketView.Test/TradeViewModelTest.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DevelopmentInProgress.Wpf.MarketView.Test
+{
+    [TestClass]
+    public class TradeViewModelTest
+    {
+        [TestMethod]
+        public void SelectedSymbol()
+        {
+            // Arrange]
+
+            // Act
+
+            // Assert
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void Quantity_and_Price()
+        {
+            // Arrange]
+
+            // Act
+
+            // Assert
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void SelectedOrderType()
+        {
+            // Arrange]
+
+            // Act
+
+            // Assert
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void IsPriceEditable()
+        {
+            // Arrange]
+
+            // Act
+
+            // Assert
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void IsMarketPrice()
+        {
+            // Arrange]
+
+            // Act
+
+            // Assert
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void IsStopLoss()
+        {
+            // Arrange]
+
+            // Act
+
+            // Assert
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void HasBaseBalance()
+        {
+            // Arrange]
+
+            // Act
+
+            // Assert
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void HasQuoteBalance()
+        {
+            // Arrange]
+
+            // Act
+
+            // Assert
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void OrderTypes()
+        {
+            // Arrange]
+
+            // Act
+
+            // Assert
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void SetSymbols()
+        {
+            // Arrange]
+
+            // Act
+
+            // Assert
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void SetAccount()
+        {
+            // Arrange]
+
+            // Act
+
+            // Assert
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void BuyQuantity()
+        {
+            // Arrange]
+
+            // Act
+
+            // Assert
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void SellQuantity()
+        {
+            // Arrange]
+
+            // Act
+
+            // Assert
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void SetQuantity()
+        {
+            // Arrange]
+
+            // Act
+
+            // Assert
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void Buy()
+        {
+            // Arrange]
+
+            // Act
+
+            // Assert
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void Sell()
+        {
+            // Arrange]
+
+            // Act
+
+            // Assert
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void SendClientOrder()
+        {
+            // Arrange]
+
+            // Act
+
+            // Assert
+            Assert.Fail();
+        }
+    }
+}
