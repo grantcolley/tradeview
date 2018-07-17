21a7cd9c1f3d387a133c13f33b2941800f487d93
grantcolley <grantcolley@yahoo.com>
2018-07-17 21:06:27 +0100

    Updating packages and references
    Updating packages and references

--- src/DevelopmentInProgress.Wpf.MarketView/Properties/AssemblyInfo.cs
+++ src/DevelopmentInProgress.Wpf.MarketView/Properties/AssemblyInfo.cs
@@ -6,10 +6,10 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("DevelopmentInProgress.MarketView")]
-[assembly: AssemblyDescription("Crypto trading")]
+[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Development In Progress Ltd")]
-[assembly: AssemblyProduct("DipTrade")]
+[assembly: AssemblyProduct("MarketView")]
 [assembly: AssemblyCopyright("Copyright ©  2018 Development In Progress Ltd")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
