b3f06f26b46fc9db12047218db1691ff84e34bf6
grantcolley <grantcolley@yahoo.com>
2018-07-24 17:47:03 +0100

    Create FileDialog.
    Create FileDialog.

--- src/DevelopmentInProgress.Wpf.StrategyManager/Model/Strategy.cs
+++ src/DevelopmentInProgress.Wpf.StrategyManager/Model/Strategy.cs
@@ -6,16 +6,32 @@ namespace DevelopmentInProgress.Wpf.StrategyManager.Model
 {
     public class Strategy : EntityBase
     {
+        private string targetAssembly;
+
         public Strategy()
         {
             StrategySubscriptions = new ObservableCollection<StrategySubscription>();
+            Dependencies = new ObservableCollection<string>();
         }
 
         public string Name { get; set; }
         public StrategyStatus Status { get; set; }
         public ObservableCollection<StrategySubscription> StrategySubscriptions { get; set; }
-        public string TargetAssembly { get; set; }
+        public ObservableCollection<string> Dependencies { get; set; }
         public string TargetType { get; set; }
         public string Tag { get; set; }
+
+        public string TargetAssembly
+        {
+            get { return targetAssembly; }
+            set
+            {
+                if(targetAssembly != value)
+                {
+                    targetAssembly = value;
+                    OnPropertyChanged("TargetAssembly");
+                }
+            }
+        }
     }
 }
