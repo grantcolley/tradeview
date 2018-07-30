b6c279a04d97ead3e0de4295f8e1a9d53aa9d4f5
grantcolley <grantcolley@yahoo.com>
2018-07-30 21:08:50 +0100

    Adding the Demo1 strategy project
    Adding the Demo1 strategy project

---
+++ strategies/DevelopmentInProgress.Demo1/Demo1Strategy.cs
@@ -0,0 +1,57 @@
+﻿using DevelopmentInProgress.MarketView.Interface.Events;
+using DevelopmentInProgress.MarketView.Interface.TradeStrategy;
+using System;
+using System.Threading.Tasks;
+
+namespace DevelopmentInProgress.Demo1
+{
+    public class Demo1Strategy : ITradeStrategy
+    {
+        public event EventHandler<TradeStrategyNotificationEventArgs> TradeStrategyNotificationEvent;
+
+        public Task<Strategy> RunAsync(Strategy strategy)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SubscribeAccountInfo(AccountInfoEventArgs accountInfoEventArgs)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SubscribeAccountInfoException(Exception exception)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SubscribeAggregateTrades(AggregateTradeEventArgs aggregateTradeEventArgs)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SubscribeAggregateTradesException(Exception exception)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SubscribeOrderBook(OrderBookEventArgs orderBookEventArgs)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SubscribeOrderBookException(Exception exception)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SubscribeStatistics(StatisticsEventArgs statisticsEventArgs)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SubscribeStatisticsException(Exception exception)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
