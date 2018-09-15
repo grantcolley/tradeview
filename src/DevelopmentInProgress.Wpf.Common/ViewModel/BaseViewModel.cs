fa983308b9e11969d2b5934c3fe027492744afae
grantcolley <grantcolley@yahoo.com>
2018-09-15 18:23:23 +0100

    Move BaseViewModel to Common
    Move BaseViewModel to Common

---
+++ src/DevelopmentInProgress.Wpf.Common/ViewModel/BaseViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows.Threading;
+
+namespace DevelopmentInProgress.Wpf.Common.ViewModel
+{
+    public abstract class BaseViewModel : IDisposable, INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public Dispatcher Dispatcher { get; set; }
+
+        public abstract void Dispose(bool disposing);
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected void OnPropertyChanged(string propertyName, bool isDirty = false)
+        {
+            var propertyChangedHandler = PropertyChanged;
+            propertyChangedHandler?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
--- src/DevelopmentInProgress.Wpf.MarketView/ViewModel/BaseViewModel.cs
+++ src/DevelopmentInProgress.Wpf.MarketView/ViewModel/BaseViewModel.cs
@@ -1,35 +1,14 @@
 ﻿using DevelopmentInProgress.Wpf.Common.Services;
-using System;
-using System.ComponentModel;
-using System.Windows.Threading;
 
 namespace DevelopmentInProgress.Wpf.MarketView.ViewModel
 {
-    public abstract class BaseViewModel : IDisposable, INotifyPropertyChanged
+    public abstract class BaseViewModel : Common.ViewModel.BaseViewModel
     {
-        public event PropertyChangedEventHandler PropertyChanged;
-
         public BaseViewModel(IWpfExchangeService exchangeService)
         {
             ExchangeService = exchangeService;
         }
 
-        public Dispatcher Dispatcher { get; set; }
-
-        public abstract void Dispose(bool disposing);
-
-        public void Dispose()
-        {
-            Dispose(true);
-            GC.SuppressFinalize(this);
-        }
-
         protected IWpfExchangeService ExchangeService { get; private set; }
-
-        protected void OnPropertyChanged(string propertyName, bool isDirty = false)
-        {
-            var propertyChangedHandler = PropertyChanged;
-            propertyChangedHandler?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
     }
 }
