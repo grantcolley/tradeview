8e93e6c8587a7016b29b8c627cc6f7292d1e06d0
grantcolley <grantcolley@yahoo.com>
2018-10-02 13:01:20 +0100

    Event handling around awaits
    Event handling around awaits

--- src/DevelopmentInProgress.Wpf.MarketView/Module.cs
+++ src/DevelopmentInProgress.Wpf.MarketView/Module.cs
@@ -45,18 +45,25 @@ namespace DevelopmentInProgress.Wpf.MarketView
 
             var accountsService = Container.Resolve<IAccountsService>();
 
-            var userAccounts = await accountsService.GetAccounts();
-
-            foreach (var userAccount in userAccounts.Accounts)
+            try
             {
-                var accountDocument = CreateAccountModuleGroupItem(userAccount.AccountName, userAccount.AccountName);
-                moduleGroup.ModuleGroupItems.Add(accountDocument);
+                var userAccounts = await accountsService.GetAccounts();
+
+                foreach (var userAccount in userAccounts.Accounts)
+                {
+                    var accountDocument = CreateAccountModuleGroupItem(userAccount.AccountName, userAccount.AccountName);
+                    moduleGroup.ModuleGroupItems.Add(accountDocument);
+                }
+
+                moduleSettings.ModuleGroups.Add(moduleGroup);
+                ModuleNavigator.AddModuleNavigation(moduleSettings);
+
+                Logger.Log("Initialize DevelopmentInProgress.Wpf.MarketView Complete", Category.Info, Priority.None);
             }
-
-            moduleSettings.ModuleGroups.Add(moduleGroup);
-            ModuleNavigator.AddModuleNavigation(moduleSettings);
-
-            Logger.Log("Initialize DevelopmentInProgress.Wpf.MarketView Complete", Category.Info, Priority.None);
+            catch(Exception ex)
+            {
+                Logger.Log($"Initialize DevelopmentInProgress.Wpf.MarketView failed to load: {ex.ToString()}", Category.Exception, Priority.None);
+            }
         }
 
         public static void AddAccount(string accountName)
