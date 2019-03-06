bcf18adc1ea3aa65f8b176065212c693035440dc
grantcolley <grantcolley@yahoo.com>
2019-03-06 21:18:21 +0000

    Add order book limits and counts to Preferences
    Add order book limits and counts to Preferences

--- src/DevelopmentInProgress.Wpf.MarketView/Model/Preferences.cs
+++ src/DevelopmentInProgress.Wpf.MarketView/Model/Preferences.cs
@@ -6,6 +6,12 @@ namespace DevelopmentInProgress.Wpf.MarketView.Model
     {
         public bool ShowFavourites { get; set; }
         public string SelectedSymbol { get; set; }
+        public int TradeLimit { get; set; }
+        public int TradesChartDisplayCount { get; set; }
+        public int TradesDisplayCount { get; set; }
+        public int OrderBookLimit { get; set; }
+        public int OrderBookChartDisplayCount { get; set; }
+        public int OrderBookDisplayCount { get; set; }
         public List<string> FavouriteSymbols { get; set; }
     }
 }
