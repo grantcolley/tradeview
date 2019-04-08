b924f8df1dd651afbb023180ff48465202499364
grantcolley <grantcolley@yahoo.com>
2019-04-08 12:46:38 +0100

    Extend ITrade interface
    Extend ITrade interface

--- src/DevelopmentInProgress.MarketView.Interface/Interfaces/ITrade.cs
+++ src/DevelopmentInProgress.MarketView.Interface/Interfaces/ITrade.cs
@@ -4,10 +4,12 @@ namespace DevelopmentInProgress.MarketView.Interface.Interfaces
 {
     public interface ITrade
     {
+        string Symbol { get; set; }
         long Id { get; set; }
         decimal Price { get; set; }
         decimal Quantity { get; set; }
         DateTime Time { get; set; }
         bool IsBuyerMaker { get; set; }
+        bool IsBestPriceMatch { get; set; }
     }
 }
