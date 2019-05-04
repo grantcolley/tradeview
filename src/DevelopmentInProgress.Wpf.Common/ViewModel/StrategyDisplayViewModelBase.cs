b7f9674a66f2be7524a08d058bd29b0283bdebae
grantcolley <grantcolley@yahoo.com>
2019-05-04 19:32:32 +0100

    Add StrategyDisplayViewModelBase
    Add StrategyDisplayViewModelBase

---
+++ src/DevelopmentInProgress.Wpf.Common/ViewModel/StrategyDisplayViewModelBase.cs
@@ -0,0 +1,55 @@
+﻿using DevelopmentInProgress.MarketView.Interface.Interfaces;
+using DevelopmentInProgress.Wpf.Common.Model;
+using Prism.Logging;
+using System;
+using System.Collections.Generic;
+using System.Windows.Threading;
+
+namespace DevelopmentInProgress.Wpf.Common.ViewModel
+{
+    public abstract class StrategyDisplayViewModelBase : BaseViewModel
+    {
+        private List<Symbol> symbols;
+        private bool isActive;
+
+        public StrategyDisplayViewModelBase(Strategy strategy, Dispatcher UiDispatcher, ILoggerFacade logger)
+            : base(logger)
+        {
+            Dispatcher = UiDispatcher;
+            Strategy = strategy;
+        }
+
+        public Strategy Strategy { get; set; }
+
+        public bool IsActive
+        {
+            get { return isActive; }
+            set
+            {
+                if (isActive != value)
+                {
+                    isActive = value;
+                    OnPropertyChanged("IsActive");
+                }
+            }
+        }
+
+        public List<Symbol> Symbols
+        {
+            get { return symbols; }
+            set
+            {
+                if (symbols != value)
+                {
+                    symbols = value;
+                    OnPropertyChanged("Symbols");
+                }
+            }
+        }
+
+        public virtual void UpdateTrades(IEnumerable<ITrade> tradesUpdate)
+        {
+            throw new NotImplementedException("StrategyDisplayViewModelBase.UpdateTrades(IEnumerable<ITrade>)");
+        }
+    }
+}
