cafb8cf462bef573192a4f1c31810de262236402
grantcolley <grantcolley@yahoo.com>
2019-05-07 20:54:59 +0100

    Create and map TradePoint for charting trade updates.
    Create and map TradePoint for charting trade updates.

--- src/DevelopmentInProgress.Wpf.Common/Chart/ChartHelper.cs
+++ src/DevelopmentInProgress.Wpf.Common/Chart/ChartHelper.cs
@@ -9,11 +9,17 @@ namespace DevelopmentInProgress.Wpf.Common.Chart
     {
         public ChartHelper()
         {
-            var baseTradeMapper = Mappers.Xy<TradeBase>()
+            var tradeBaseMapper = Mappers.Xy<TradeBase>()
                 .X(model => model.Time.Ticks)
                 .Y(model => Convert.ToDouble(model.Price));
 
-            Charting.For<TradeBase>(baseTradeMapper);
+            Charting.For<TradeBase>(tradeBaseMapper);
+
+            var tradePointMapper = Mappers.Xy<TradePoint>()
+                .X(model => model.Time.Ticks)
+                .Y(model => Convert.ToDouble(model.Price));
+
+            Charting.For<TradePoint>(tradePointMapper);
         }
 
         public Func<double, string> TimeFormatter => value => new DateTime((long)value).ToString("H:mm:ss");
