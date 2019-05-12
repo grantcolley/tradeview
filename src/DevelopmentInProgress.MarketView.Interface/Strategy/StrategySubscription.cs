e179b97211c6eeaeecb0b3d17126fa9f75837221
grantcolley <grantcolley@yahoo.com>
2019-05-12 21:28:47 +0100

    Add NotionalMinimumValue to StrategySubscription
    Add NotionalMinimumValue to StrategySubscription

--- src/DevelopmentInProgress.MarketView.Interface/Strategy/StrategySubscription.cs
+++ src/DevelopmentInProgress.MarketView.Interface/Strategy/StrategySubscription.cs
@@ -3,6 +3,7 @@
     public class StrategySubscription
     {
         public string Symbol { get; set; }
+        public decimal NotionalMinimumValue { get; set; }
         public int Limit { get; set; }
         public string ApiKey { get; set; }
         public string SecretKey { get; set; }
