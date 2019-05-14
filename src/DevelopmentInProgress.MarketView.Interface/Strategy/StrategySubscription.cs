b11c9c91b68e1ea000a76404e052c7f306b8f5c7
grantcolley <grantcolley@yahoo.com>
2019-05-14 21:27:46 +0100

    Add strategySubscriptions to tradeStrategy.AddExchangeService and remove NotionalMinimumValue from StrategySubscription

--- src/DevelopmentInProgress.MarketView.Interface/Strategy/StrategySubscription.cs
+++ src/DevelopmentInProgress.MarketView.Interface/Strategy/StrategySubscription.cs
@@ -3,7 +3,6 @@
     public class StrategySubscription
     {
         public string Symbol { get; set; }
-        public decimal NotionalMinimumValue { get; set; }
         public int Limit { get; set; }
         public string ApiKey { get; set; }
         public string SecretKey { get; set; }
