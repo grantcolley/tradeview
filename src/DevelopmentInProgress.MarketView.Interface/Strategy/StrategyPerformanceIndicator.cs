07e4e69a81fa539b24abd8ed61b7775d4ae9a690
grantcolley <grantcolley@yahoo.com>
2019-05-16 15:26:35 +0100

    Move StrategyPerformanceIndicator to DevelopmentInProgress.MarketView.Interface
    Move StrategyPerformanceIndicator to DevelopmentInProgress.MarketView.Interface

---
+++ src/DevelopmentInProgress.MarketView.Interface/Strategy/StrategyPerformanceIndicator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DevelopmentInProgress.MarketView.Interface.Strategy
+{
+    public class StrategyPerformanceIndicator
+    {
+        public string StrategyName { get; set; }
+        public double SessionId { get; set; }
+        public DateTime Date { get; set; }
+        public decimal Quantity { get; set; }
+        public decimal Performance { get; set; }
+    }
+}
