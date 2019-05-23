76237ed5179709c35198408c272a35bfac3edb70
grantcolley <grantcolley@yahoo.com>
2019-05-23 22:15:47 +0100

    Add test project for SMA
    Add test project for SMA

---
+++ test/DevelopmentInProgress.SMA.Test/SmaHelperTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DevelopmentInProgress.SMA.Test
+{
+    [TestClass]
+    public class SmaHelperTest
+    {
+        [TestMethod]
+        public void CalculateMovingVolatility()
+        {
+            // Arrange
+            var deviationsSquared = new double[] { 0, 4, 25, 4, 9, 25, 0, 1, 16, 4, 16, 0, 9, 25, 4, 9, 9, 4, 1, 4, 9 };
+
+            // Act
+            var volatility = SmaHelper.CalculateMovingVolatility(20, deviationsSquared, 20);
+
+            // Assert
+            Assert.AreEqual(Math.Round(volatility, 3), 3.061);
+        }
+    }
+}
