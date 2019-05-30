ad81c73f994403b8f37cb63a828af7c6e3508676
grantcolley <grantcolley@yahoo.com>
2019-05-30 08:22:07 +0100

    Create StrategyPerformanceIndicator.cs

---
+++ src/DevelopmentInProgress.MarketView.Interface/Strategy/StrategyPerformanceIndicator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DevelopmentInProgress.MarketView.Interface.Strategy
+{
+    public class StrategyPerformanceIndicator
+    {
+        public string Symbol { get; set; }
+        public string BaseAsset { get; set; }
+        public decimal BaseQuantity { get; set; }
+        public string QuoteAsset { get; set; }
+        public decimal QuoteQuantity { get; set; }
+        public decimal IndicativePrice { get; set; }
+        public DateTime IndicationPriceTime { get; set; }
+    }
+}
