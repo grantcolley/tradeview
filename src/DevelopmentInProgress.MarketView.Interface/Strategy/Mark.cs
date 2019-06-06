0ccc6506371d481ee442c88a694e2588198fe612
grantcolley <grantcolley@yahoo.com>
2019-06-06 20:40:19 +0100

    Create Marks for StrategySymbolPerformance
    Create Marks for StrategySymbolPerformance

---
+++ src/DevelopmentInProgress.MarketView.Interface/Strategy/Mark.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DevelopmentInProgress.MarketView.Interface.Strategy
+{
+    public class Mark
+    {
+        public string BaseAsset { get; set; }
+        public decimal BaseQuantity { get; set; }
+        public string QuoteAsset { get; set; }
+        public decimal QuoteQuantity { get; set; }
+        public decimal Price { get; set; }
+        public decimal MarkValue { get; set; }
+        public DateTime MarkTime { get; set; }
+    }
+}
--- src/DevelopmentInProgress.MarketView.Interface/Strategy/StrategySymbolPerformance.cs
+++ src/DevelopmentInProgress.MarketView.Interface/Strategy/StrategySymbolPerformance.cs
@@ -7,16 +7,12 @@ namespace DevelopmentInProgress.MarketView.Interface.Strategy
     {
         public StrategySymbolPerformance()
         {
+            Marks = new List<Mark>();
             Trades = new List<AccountTrade>();
         }
 
         public string Symbol { get; set; }
-        public string BaseAsset { get; set; }
-        public decimal BaseQuantity { get; set; }
-        public string QuoteAsset { get; set; }
-        public decimal QuoteQuantity { get; set; }
-        public decimal Price { get; set; }
-        public decimal Value { get; set; }
+        public List<Mark> Marks { get; set; }
         public List<AccountTrade> Trades { get; set; }
     }
 }
