656618d97207a4a5d12aac5d7afec0919cc347c9
grantcolley <grantcolley@yahoo.com>
2019-06-14 09:07:57 +0100

    Update SymbolsExtensions
    Update SymbolsExtensions

--- src/DevelopmentInProgress.Wpf.Common/Extensions/SymbolExtensions.cs
+++ src/DevelopmentInProgress.Wpf.Common/Extensions/SymbolExtensions.cs
@@ -45,6 +45,26 @@ namespace DevelopmentInProgress.Wpf.Common.Extensions
             sy.LastPriceChangeDirection = st.LastPrice > sy.SymbolStatistics.LastPrice ? 1 : st.LastPrice < sy.SymbolStatistics.LastPrice ? -1 : 0;
             sy.SymbolStatistics.LastPrice = st.LastPrice.Trim(sy.PricePrecision);
             sy.SymbolStatistics.Volume = Convert.ToInt64(st.Volume);
+
+            sy.SymbolStatistics.FirstTradeId = st.FirstTradeId;
+            sy.SymbolStatistics.CloseTime = st.CloseTime;
+            sy.SymbolStatistics.OpenTime = st.OpenTime;
+            sy.SymbolStatistics.QuoteVolume = st.QuoteVolume;
+            sy.SymbolStatistics.LowPrice = st.LowPrice;
+            sy.SymbolStatistics.HighPrice = st.HighPrice;
+            sy.SymbolStatistics.OpenPrice = st.OpenPrice;
+            sy.SymbolStatistics.AskQuantity = st.AskQuantity;
+            sy.SymbolStatistics.AskPrice = st.AskPrice;
+            sy.SymbolStatistics.BidQuantity = st.BidQuantity;
+            sy.SymbolStatistics.BidPrice = st.BidPrice;
+            sy.SymbolStatistics.LastQuantity = st.LastQuantity;
+            sy.SymbolStatistics.PreviousClosePrice = st.PreviousClosePrice;
+            sy.SymbolStatistics.WeightedAveragePrice = st.WeightedAveragePrice;
+            sy.SymbolStatistics.PriceChange = st.PriceChange;
+            sy.SymbolStatistics.Period = st.Period;
+            sy.SymbolStatistics.LastTradeId = st.LastTradeId;
+            sy.SymbolStatistics.TradeCount = st.TradeCount;
+
             return sy;
         }
 
