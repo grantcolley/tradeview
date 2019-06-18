f828c39b663aa1ea20a501c070585f3c6ba8a2df
grantcolley <grantcolley@yahoo.com>
2019-06-18 20:26:34 +0100

    Add Candlestick and CandlestickInterval
    Add Candlestick and CandlestickInterval

---
+++ src/DevelopmentInProgress.MarketView.Interface/Model/Candlestick.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace DevelopmentInProgress.MarketView.Interface.Model
+{
+    public class Candlestick
+    {
+        public string Symbol { get; }
+        public CandlestickInterval Interval { get; }
+        public DateTime OpenTime { get; }
+        public decimal Open { get; }
+        public decimal High { get; }
+        public decimal Low { get; }
+        public decimal Close { get; }
+        public decimal Volume { get; }
+        public DateTime CloseTime { get; }
+        public decimal QuoteAssetVolume { get; }
+        public long NumberOfTrades { get; }
+        public decimal TakerBuyBaseAssetVolume { get; }
+        public decimal TakerBuyQuoteAssetVolume { get; }
+    }
+}
