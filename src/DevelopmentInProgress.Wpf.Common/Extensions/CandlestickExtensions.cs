5fa9b9ee023227873f7a6e59ed92a5c393891df0
grantcolley <grantcolley@yahoo.com>
2019-06-21 13:35:42 +0100

    Add CandlestickExtensions and property changes to Candlestick
    Add CandlestickExtensions and property changes to Candlestick

---
+++ src/DevelopmentInProgress.Wpf.Common/Extensions/CandlestickExtensions.cs
@@ -0,0 +1,27 @@
+﻿using DevelopmentInProgress.Wpf.Common.Model;
+
+namespace DevelopmentInProgress.Wpf.Common.Extensions
+{
+    public static class CandlestickExtensions
+    {
+        public static Candlestick GetViewCandlestick(this MarketView.Interface.Model.Candlestick c)
+        {
+            return new Candlestick
+            {
+                Symbol = c.Symbol,
+                Interval = c.Interval,
+                OpenTime = c.OpenTime,
+                Open = c.Open,
+                High = c.High,
+                Low = c.Low,
+                Close = c.Close,
+                Volume = c.Volume,
+                CloseTime = c.CloseTime,
+                QuoteAssetVolume = c.QuoteAssetVolume,
+                NumberOfTrades = c.NumberOfTrades,
+                TakerBuyBaseAssetVolume = c.TakerBuyBaseAssetVolume,
+                TakerBuyQuoteAssetVolume = c.TakerBuyQuoteAssetVolume
+            };
+        }
+    }
+}
