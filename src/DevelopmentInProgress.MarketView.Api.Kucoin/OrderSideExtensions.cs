79c01b06ac193c984b08cb03bc380fb2932a4e7a
grantcolley <grantcolley@yahoo.com>
2019-10-05 19:44:37 +0100

    Add order extensions
    Add order extensions

---
+++ src/DevelopmentInProgress.MarketView.Api.Kucoin/OrderSideExtensions.cs
@@ -0,0 +1,35 @@
+﻿using DevelopmentInProgress.MarketView.Interface.Model;
+using Kucoin.Net.Objects;
+using System;
+
+namespace DevelopmentInProgress.MarketView.Api.Kucoin
+{
+    public static class OrderSideExtensions
+    {
+        public static KucoinOrderSide ToKucoinOrderSide(this OrderSide order)
+        {
+            switch (order)
+            {
+                case OrderSide.Buy:
+                    return KucoinOrderSide.Buy;
+                case OrderSide.Sell:
+                    return KucoinOrderSide.Sell;
+                default:
+                    throw new NotImplementedException();
+            }
+        }
+
+        public static OrderSide ToMarketViewOrderSide(this KucoinOrderSide order)
+        {
+            switch (order)
+            {
+                case KucoinOrderSide.Buy:
+                    return OrderSide.Buy;
+                case KucoinOrderSide.Sell:
+                    return OrderSide.Sell;
+                default:
+                    throw new NotImplementedException();
+            }
+        }
+    }
+}
---
+++ src/DevelopmentInProgress.MarketView.Api.Kucoin/OrderTypeExtensions.cs
@@ -0,0 +1,39 @@
+﻿using DevelopmentInProgress.MarketView.Interface.Model;
+using Kucoin.Net.Objects;
+using System;
+
+namespace DevelopmentInProgress.MarketView.Api.Kucoin
+{
+    public static class OrderTypeExtensions
+    {
+        public static KucoinOrderType ToKucoinOrderType(this OrderType order)
+        {
+            switch (order)
+            {
+                case OrderType.Limit:
+                    return KucoinOrderType.Limit;
+                case OrderType.StopLossLimit:
+                    return KucoinOrderType.LimitStop;
+                case OrderType.Market:
+                    return KucoinOrderType.Market;
+                default:
+                    throw new NotImplementedException();
+            }
+        }
+
+        public static OrderType ToMarketViewOrderType(this KucoinOrderType order)
+        {
+            switch (order)
+            {
+                case KucoinOrderType.Limit:
+                    return OrderType.Limit;
+                case KucoinOrderType.LimitStop:
+                    return OrderType.StopLossLimit;
+                case KucoinOrderType.Market:
+                    return OrderType.Market;
+                default:
+                    throw new NotImplementedException();
+            }
+        }
+    }
+}
