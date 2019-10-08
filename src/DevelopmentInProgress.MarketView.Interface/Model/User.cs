fd1bcf4dfe357579f0a8b5781e4c4c7a63da1e28
grantcolley <grantcolley@yahoo.com>
2019-10-08 15:00:49 +0100

    Update User.cs

--- src/DevelopmentInProgress.MarketView.Interface/Model/User.cs
+++ src/DevelopmentInProgress.MarketView.Interface/Model/User.cs
@@ -4,6 +4,7 @@
     {
         public string ApiKey { get; set; }
         public string ApiSecret { get; set; }
+        public string ApiPassPhrase { get; set; }
         public RateLimiter RateLimiter { get; set; }
     }
 }
