dfe614fd5f931e90bfe751b001ba7fef52e9f28d
grantcolley <grantcolley@yahoo.com>
2019-10-10 20:42:12 +0100

    Create ISymbolsCacheFactory and SymbolsCacheFactory
    Create ISymbolsCacheFactory and SymbolsCacheFactory

---
+++ src/DevelopmentInProgress.Wpf.Common/Cache/ISymbolsCacheFactory.cs
@@ -0,0 +1,9 @@
+﻿using DevelopmentInProgress.MarketView.Interface.Enums;
+
+namespace DevelopmentInProgress.Wpf.Common.Cache
+{
+    public interface ISymbolsCacheFactory
+    {
+        ISymbolsCache GetSymbolsCache(Exchange exchange);
+    }
+}
---
+++ src/DevelopmentInProgress.Wpf.Common/Cache/SymbolsCacheFactory.cs
@@ -0,0 +1,45 @@
+﻿using DevelopmentInProgress.MarketView.Interface.Enums;
+using DevelopmentInProgress.MarketView.Interface.Interfaces;
+using DevelopmentInProgress.MarketView.Service;
+using DevelopmentInProgress.Wpf.Common.Services;
+using System.Collections.Generic;
+
+namespace DevelopmentInProgress.Wpf.Common.Cache
+{
+    public class SymbolsCacheFactory : ISymbolsCacheFactory
+    {
+        private IExchangeApiFactory exchangeApiFactory;
+        private Dictionary<Exchange, ISymbolsCache> exchangeSymbolsCache;
+        private object exchangeSymbolsCacheLock = new object();
+
+        public SymbolsCacheFactory(IExchangeApiFactory exchangeApiFactory)
+        {
+            this.exchangeApiFactory = exchangeApiFactory;
+            exchangeSymbolsCache = new Dictionary<Exchange, ISymbolsCache>();
+        }
+
+        public ISymbolsCache GetSymbolsCache(Exchange exchange)
+        {
+            if (exchangeSymbolsCache.ContainsKey(exchange))
+            {
+                return exchangeSymbolsCache[exchange];
+            }
+            else
+            {
+                lock (exchangeSymbolsCacheLock)
+                {
+                    if (exchangeSymbolsCache.ContainsKey(exchange))
+                    {
+                        return exchangeSymbolsCache[exchange];
+                    }
+                    else
+                    {
+                        var wpfExchangeService = new WpfExchangeService(new ExchangeService(exchangeApiFactory));
+                        var symbolsCache = new SymbolsCache(exchange, wpfExchangeService);
+                        return symbolsCache;
+                    }
+                }
+            }
+        }
+    }
+}
