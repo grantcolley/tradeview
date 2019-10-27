47ec7964e72d4117c96cb539983b69b3165bc823
grantcolley <grantcolley@yahoo.com>
2019-10-27 20:29:37 +0000

    Add KucoinExchangeTestApiEnum
    Add KucoinExchangeTestApiEnum

---
+++ test/DevelopmentInProgress.MarketView.Test.Helper/KucoinExchangeTestApiEnum.cs
@@ -0,0 +1,9 @@
+﻿namespace DevelopmentInProgress.MarketView.Test.Helper
+{
+    public enum KucoinExchangeTestApiEnum
+    {
+        KucoinApiExample = 0,
+        CreateLocalOrderBookExample = 1,
+        UpdateLocalOrderBookExample = 2
+    }
+}
