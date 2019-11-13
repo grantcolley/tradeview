93f74c166ed8e8c7bab41f1567e168342484989e
grantcolley <grantcolley@yahoo.com>
2019-11-13 20:45:43 +0000

    Update OrderBookUpdateHelper.cs

--- test/DevelopmentInProgress.MarketView.Test.Helper/OrderBookUpdateHelper.cs
+++ test/DevelopmentInProgress.MarketView.Test.Helper/OrderBookUpdateHelper.cs
@@ -6,40 +6,7 @@ namespace DevelopmentInProgress.MarketView.Test.Helper
 {
     public static class OrderBookUpdateHelper
     {
-        public static OrderBook OrderBook_Trx_GetFirstUpdate_1()
-        {
-            var ask0 = new OrderBookPriceLevel { Price = 1.11m, Quantity = 170 };
-            var ask1 = new OrderBookPriceLevel { Price = 1.12m, Quantity = 100 };
-            var ask2 = new OrderBookPriceLevel { Price = 1.15m, Quantity = 200 };
-            var ask3 = new OrderBookPriceLevel { Price = 1.16m, Quantity = 150 };
-            var ask4 = new OrderBookPriceLevel { Price = 1.18m, Quantity = 110 };
-            var ask5 = new OrderBookPriceLevel { Price = 1.20m, Quantity = 170 };
-            var ask6 = new OrderBookPriceLevel { Price = 1.21m, Quantity = 100 };
-            var ask7 = new OrderBookPriceLevel { Price = 1.22m, Quantity = 200 };
-            var ask8 = new OrderBookPriceLevel { Price = 1.23m, Quantity = 150 };
-            var ask9 = new OrderBookPriceLevel { Price = 1.24m, Quantity = 110 };
-
-            var bid0 = new OrderBookPriceLevel { Price = 1.08m, Quantity = 180 };
-            var bid1 = new OrderBookPriceLevel { Price = 1.07m, Quantity = 120 };
-            var bid2 = new OrderBookPriceLevel { Price = 1.05m, Quantity = 130 };
-            var bid3 = new OrderBookPriceLevel { Price = 1.04m, Quantity = 140 };
-            var bid4 = new OrderBookPriceLevel { Price = 1.01m, Quantity = 160 };
-            var bid5 = new OrderBookPriceLevel { Price = 1.00m, Quantity = 180 };
-            var bid6 = new OrderBookPriceLevel { Price = 0.99m, Quantity = 120 };
-            var bid7 = new OrderBookPriceLevel { Price = 0.96m, Quantity = 130 };
-            var bid8 = new OrderBookPriceLevel { Price = 0.95m, Quantity = 140 };
-            var bid9 = new OrderBookPriceLevel { Price = 0.94m, Quantity = 160 };
-
-            var orderBook = new OrderBook();
-            orderBook.Symbol = "TRXBTC";
-            orderBook.LastUpdateId = DateTime.Now.Ticks;
-            orderBook.Asks = new List<OrderBookPriceLevel>(new[] { ask0, ask1, ask2, ask3, ask4, ask5, ask6, ask7, ask8, ask9 });
-            orderBook.Bids = new List<OrderBookPriceLevel>(new[] { bid0, bid1, bid2, bid3, bid4, bid5, bid6, bid7, bid8, bid9 });
-
-            return orderBook;
-        }
-
-        public static OrderBook OrderBook_Trx_GetFirstUpdate_2()
+        public static OrderBook OrderBook_Trx_GetFirstUpdate()
         {
             var ask0 = new OrderBookPriceLevel { Price = 1.11m, Quantity = 170 };
             var ask1 = new OrderBookPriceLevel { Price = 1.12m, Quantity = 100 };
