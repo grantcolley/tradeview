5998cf628a00f0030cd5de3f2b1501e5623de998
grantcolley <grantcolley@yahoo.com>
2019-11-18 20:52:35 +0000

    Add SymbolName
    Add SymbolName

---
+++ src/DevelopmentInProgress.Wpf.Common/Model/SymbolName.cs
@@ -0,0 +1,8 @@
+﻿namespace DevelopmentInProgress.Wpf.Common.Model
+{
+    public class SymbolName : EntityBase
+    {
+        public string Name { get; set; }
+        public bool IsFavourite { get; set; }
+    }
+}
