737d929086106859eade20b941db6841a7eb4f5e
grantcolley <grantcolley@yahoo.com>
2019-11-18 20:39:03 +0000

    Add UserAccountEventArgs
    Add UserAccountEventArgs

---
+++ src/DevelopmentInProgress.Wpf.Configuration/Events/UserAccountEventArgs.cs
@@ -0,0 +1,9 @@
+﻿using DevelopmentInProgress.Wpf.Common.Events;
+using DevelopmentInProgress.Wpf.Common.Model;
+
+namespace DevelopmentInProgress.Wpf.Configuration.Events
+{
+    public class UserAccountEventArgs : EventArgsBase<UserAccount>
+    {
+    }
+}
