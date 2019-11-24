d575839c0c5598f3c6773369a15130469dcd2ff1
grantcolley <grantcolley@yahoo.com>
2019-11-24 12:59:39 +0000

    Create UserAccount, Preferences and UserAccounts
    Create UserAccount, Preferences and UserAccounts

---
+++ src/DevelopmentInProgress.TradeView.Interface/Model/UserAccounts.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace DevelopmentInProgress.TradeView.Interface.Model
+{
+    public class UserAccounts
+    {
+        public UserAccounts()
+        {
+            Accounts = new List<UserAccount>();
+        }
+
+        public List<UserAccount> Accounts { get; set; }
+    }
+}
