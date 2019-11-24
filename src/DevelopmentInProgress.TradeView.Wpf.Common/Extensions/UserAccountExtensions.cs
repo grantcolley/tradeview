8624dd2de9e5864cf7165ebb290ff3849ffb3ed9
grantcolley <grantcolley@yahoo.com>
2019-11-24 21:23:59 +0000

    Use Data for persisting user accounts
    Use Data for persisting user accounts

---
+++ src/DevelopmentInProgress.TradeView.Wpf.Common/Extensions/UserAccountExtensions.cs
@@ -0,0 +1,61 @@
+﻿using DevelopmentInProgress.TradeView.Wpf.Common.Model;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace DevelopmentInProgress.TradeView.Wpf.Common.Extensions
+{
+    public static class UserAccountExtensions
+    {
+        public static UserAccount ToUserAccount(this Interface.Model.UserAccount ua)
+        {
+            var userAccount = new UserAccount
+            {
+                AccountName = ua.AccountName,
+                ApiKey = ua.ApiKey,
+                ApiSecret = ua.ApiSecret,
+                ApiPassPhrase = ua.ApiPassPhrase,
+                Exchange = ua.Exchange,
+                Preferences = new Preferences
+                {
+                    SelectedSymbol = ua.Preferences.SelectedSymbol,
+                    ShowAggregateTrades = ua.Preferences.ShowAggregateTrades,
+                    TradeLimit = ua.Preferences.TradeLimit,
+                    TradesChartDisplayCount = ua.Preferences.TradesChartDisplayCount,
+                    TradesDisplayCount = ua.Preferences.TradesDisplayCount,
+                    OrderBookLimit = ua.Preferences.OrderBookLimit,
+                    OrderBookChartDisplayCount = ua.Preferences.OrderBookChartDisplayCount,
+                    OrderBookDisplayCount = ua.Preferences.OrderBookDisplayCount,
+                    FavouriteSymbols = new ObservableCollection<string>(ua.Preferences.FavouriteSymbols)
+                }
+            };
+
+            return userAccount;
+        }
+
+        public static Interface.Model.UserAccount ToInterfaceUserAccount(this UserAccount ua)
+        {
+            var userAccount = new Interface.Model.UserAccount
+            {
+                AccountName = ua.AccountName,
+                ApiKey = ua.ApiKey,
+                ApiSecret = ua.ApiSecret,
+                ApiPassPhrase = ua.ApiPassPhrase,
+                Exchange = ua.Exchange,
+                Preferences = new Interface.Model.Preferences
+                {
+                    SelectedSymbol = ua.Preferences.SelectedSymbol,
+                    ShowAggregateTrades = ua.Preferences.ShowAggregateTrades,
+                    TradeLimit = ua.Preferences.TradeLimit,
+                    TradesChartDisplayCount = ua.Preferences.TradesChartDisplayCount,
+                    TradesDisplayCount = ua.Preferences.TradesDisplayCount,
+                    OrderBookLimit = ua.Preferences.OrderBookLimit,
+                    OrderBookChartDisplayCount = ua.Preferences.OrderBookChartDisplayCount,
+                    OrderBookDisplayCount = ua.Preferences.OrderBookDisplayCount,
+                    FavouriteSymbols = ua.Preferences.FavouriteSymbols.ToList()
+                }
+            };
+
+            return userAccount;
+        }
+    }
+}
--- src/DevelopmentInProgress.TradeView.Wpf.Common/Services/AccountsService.cs
+++ src/DevelopmentInProgress.TradeView.Wpf.Common/Services/AccountsService.cs
@@ -1,4 +1,6 @@
-﻿using DevelopmentInProgress.TradeView.Wpf.Common.Model;
+﻿using DevelopmentInProgress.TradeView.Data;
+using DevelopmentInProgress.TradeView.Wpf.Common.Extensions;
+using DevelopmentInProgress.TradeView.Wpf.Common.Model;
 using Newtonsoft.Json;
 using System;
 using System.IO;
@@ -7,102 +9,40 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace DevelopmentInProgress.TradeView.Wpf.Common.Services
-{
+{ 
     public class AccountsService : IAccountsService
     {
+        private ITradeViewConfigurationAccounts accounts;
+
+        public AccountsService(ITradeViewData tradeViewData)
+        {
+            accounts = tradeViewData.ConfigurationData.Accounts;
+        }
+
         public async Task<UserAccounts> GetAccountsAsync()
         {
-            if (File.Exists(userAccountsFile))
-            {
-                using (var reader = File.OpenText(userAccountsFile))
-                {
-                    var json = await reader.ReadToEndAsync();
-                    return JsonConvert.DeserializeObject<UserAccounts>(json);
-                }
-            }
+            var result = await accounts.GetAccountsAsync();
 
-            return new UserAccounts();
+            return new UserAccounts
+            {
+                Accounts = result.Accounts.Select(ua => ua.ToUserAccount()).ToList()
+            };
         }
 
         public async Task<UserAccount> GetAccountAsync(string accountName)
         {
-            if (File.Exists(userAccountsFile))
-            {
-                string json;
-                using (var reader = File.OpenText(userAccountsFile))
-                {
-                    json = await reader.ReadToEndAsync();
-                }
-
-                var userAccounts = JsonConvert.DeserializeObject<UserAccounts>(json);
-                var userAccount = userAccounts.Accounts.Single(a => a.AccountName.Equals(accountName));
-                return userAccount;
-            }
-
-            throw new Exception($"Account {accountName} not available.");
+            var result = await accounts.GetAccountAsync(accountName);
+            return result.ToUserAccount();
         }
 
-        public async Task SaveAccountAsync(UserAccount userAccount)
+        public Task SaveAccountAsync(UserAccount userAccount)
         {
-            UserAccounts userAccounts;
-
-            if (File.Exists(userAccountsFile))
-            {
-                using (var reader = File.OpenText(userAccountsFile))
-                {
-                    var rjson = await reader.ReadToEndAsync();
-                    userAccounts = JsonConvert.DeserializeObject<UserAccounts>(rjson);
-                }
-            }
-            else
-            {
-                userAccounts = new UserAccounts();
-            }
-
-            var dupe = userAccounts.Accounts.FirstOrDefault(a => a.AccountName.Equals(userAccount.AccountName));
-            if (dupe != null)
-            {
-                userAccounts.Accounts.Remove(dupe);
-            }
-
-            userAccounts.Accounts.Add(userAccount);
-
-            var wjson = JsonConvert.SerializeObject(userAccounts, Formatting.Indented);
-
-            UnicodeEncoding encoding = new UnicodeEncoding();
-            char[] chars = encoding.GetChars(encoding.GetBytes(wjson));
-            using (StreamWriter writer = File.CreateText(userAccountsFile))
-            {
-                await writer.WriteAsync(chars, 0, chars.Length);
-            }
+            return accounts.SaveAccountAsync(userAccount.ToInterfaceUserAccount());
         }
 
-        public async Task DeleteAccountAsync(UserAccount userAccount)
+        public Task DeleteAccountAsync(UserAccount userAccount)
         {
-            if (File.Exists(userAccountsFile))
-            {
-                UserAccounts userAccounts;
-
-                using (var reader = File.OpenText(userAccountsFile))
-                {
-                    var rjson = await reader.ReadToEndAsync();
-                    userAccounts = JsonConvert.DeserializeObject<UserAccounts>(rjson);
-                }
-
-                var remove = userAccounts.Accounts.FirstOrDefault(a => a.AccountName.Equals(userAccount.AccountName));
-                if (remove != null)
-                {
-                    userAccounts.Accounts.Remove(remove);
-                    var wjson = JsonConvert.SerializeObject(userAccounts, Formatting.Indented);
-
-                    UnicodeEncoding encoding = new UnicodeEncoding();
-                    char[] chars = encoding.GetChars(encoding.GetBytes(wjson));
-                    using (StreamWriter writer = File.CreateText(userAccountsFile))
-                    {
-                        await writer.WriteAsync(chars, 0, chars.Length);
-                    }
-                }
-            }
+            return accounts.DeleteAccountAsync(userAccount.ToInterfaceUserAccount());
         }
     }
 }
