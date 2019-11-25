b6c3be425704f5459d7f564e6c8ce26cd1b8829f
grantcolley <grantcolley@yahoo.com>
2019-11-25 20:17:12 +0000

    Update AccountsService.cs

--- src/DevelopmentInProgress.TradeView.Wpf.Common/Services/AccountsService.cs
+++ src/DevelopmentInProgress.TradeView.Wpf.Common/Services/AccountsService.cs
@@ -1,27 +1,23 @@
 ﻿using DevelopmentInProgress.TradeView.Data;
 using DevelopmentInProgress.TradeView.Wpf.Common.Extensions;
 using DevelopmentInProgress.TradeView.Wpf.Common.Model;
-using Newtonsoft.Json;
-using System;
-using System.IO;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace DevelopmentInProgress.TradeView.Wpf.Common.Services
 { 
     public class AccountsService : IAccountsService
     {
-        private ITradeViewConfigurationAccounts accounts;
+        private ITradeViewConfigurationAccounts configurationAccounts;
 
         public AccountsService(ITradeViewData tradeViewData)
         {
-            accounts = tradeViewData.ConfigurationData.Accounts;
+            configurationAccounts = tradeViewData.ConfigurationData.Accounts;
         }
 
         public async Task<UserAccounts> GetAccountsAsync()
         {
-            var result = await accounts.GetAccountsAsync();
+            var result = await configurationAccounts.GetAccountsAsync();
 
             return new UserAccounts
             {
@@ -31,18 +27,18 @@ namespace DevelopmentInProgress.TradeView.Wpf.Common.Services
 
         public async Task<UserAccount> GetAccountAsync(string accountName)
         {
-            var result = await accounts.GetAccountAsync(accountName);
+            var result = await configurationAccounts.GetAccountAsync(accountName);
             return result.ToUserAccount();
         }
 
         public Task SaveAccountAsync(UserAccount userAccount)
         {
-            return accounts.SaveAccountAsync(userAccount.ToInterfaceUserAccount());
+            return configurationAccounts.SaveAccountAsync(userAccount.ToInterfaceUserAccount());
         }
 
         public Task DeleteAccountAsync(UserAccount userAccount)
         {
-            return accounts.DeleteAccountAsync(userAccount.ToInterfaceUserAccount());
+            return configurationAccounts.DeleteAccountAsync(userAccount.ToInterfaceUserAccount());
         }
     }
 }
