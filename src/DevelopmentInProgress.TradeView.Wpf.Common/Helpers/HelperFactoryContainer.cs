047d69d258cb991850e1436fb1c8539722101259
grantcolley <grantcolley@yahoo.com>
2020-01-12 20:37:16 +0000

    Create IHelperFactoryContainer
    Create IHelperFactoryContainer

---
+++ src/DevelopmentInProgress.TradeView.Wpf.Common/Helpers/HelperFactoryContainer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DevelopmentInProgress.TradeView.Wpf.Common.Helpers
+{
+    public class HelperFactoryContainer : IHelperFactoryContainer
+    {
+        private Dictionary<Type, IHelperFactory> factories;
+
+        public HelperFactoryContainer(ITradeHelperFactory tradeHelperFactory, IOrderBookHelperFactory orderBookHelperFactory)
+        {
+            factories = new Dictionary<Type, IHelperFactory>();
+            factories.Add(typeof(ITradeHelperFactory), tradeHelperFactory);
+            factories.Add(typeof(IOrderBookHelperFactory), orderBookHelperFactory);
+        }
+
+        public IHelperFactory GetFactory<T>() where T : IHelperFactory
+        {
+            return factories[typeof(T)];
+        }
+    }
+}
---
+++ src/DevelopmentInProgress.TradeView.Wpf.Common/Helpers/IHelperFactoryContainer.cs
@@ -0,0 +1,7 @@
+﻿namespace DevelopmentInProgress.TradeView.Wpf.Common.Helpers
+{
+    public interface IHelperFactoryContainer
+    {
+        IHelperFactory GetFactory<T>() where T : IHelperFactory;
+    }
+}
