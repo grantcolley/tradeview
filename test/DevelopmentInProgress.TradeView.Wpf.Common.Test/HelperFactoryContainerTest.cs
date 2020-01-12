3339d1fdeb9042082ca093897c87b0ff154e17ea
grantcolley <grantcolley@yahoo.com>
2020-01-12 20:37:57 +0000

    Create HelperFactoryContainerTest
    Create HelperFactoryContainerTest

---
+++ test/DevelopmentInProgress.TradeView.Wpf.Common.Test/HelperFactoryContainerTest.cs
@@ -0,0 +1,27 @@
+﻿using DevelopmentInProgress.TradeView.Service;
+using DevelopmentInProgress.TradeView.Wpf.Common.Helpers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DevelopmentInProgress.TradeView.Wpf.Common.Test
+{
+    [TestClass]
+    public class HelperFactoryContainerTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            // Arrange
+            var helperFactoryContainer = new HelperFactoryContainer(
+                new TradeHelperFactory(new ExchangeApiFactory()),
+                new OrderBookHelperFactory(new ExchangeApiFactory()));
+
+            // Act
+            var factory1 = helperFactoryContainer.GetFactory<ITradeHelperFactory>();
+            var factory2 = helperFactoryContainer.GetFactory<IOrderBookHelperFactory>();
+
+            // Assert
+            Assert.IsInstanceOfType(factory1, typeof(TradeHelperFactory));
+            Assert.IsInstanceOfType(factory2, typeof(OrderBookHelperFactory));
+        }
+    }
+}
