e78ec18284adeac04936844e192c32017d0c9d70
grantcolley <grantcolley@yahoo.com>
2020-01-14 18:21:58 +0000

    Update AggregateTrade.cs

--- src/DevelopmentInProgress.TradeView.Wpf.Common/Model/AggregateTrade.cs
+++ src/DevelopmentInProgress.TradeView.Wpf.Common/Model/AggregateTrade.cs
@@ -2,8 +2,6 @@
 {
     public class AggregateTrade : TradeBase
     {
-        public string Symbol { get; set; }
-        public bool IsBestPriceMatch { get; set; }
         public long FirstTradeId { get; set; }
         public long LastTradeId { get; set; }
     }
