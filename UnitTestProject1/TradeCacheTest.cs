bebf1a62fe3e523b3071e5bddf118c1d83a8d3d3
grantcolley <grantcolley@yahoo.com>
2020-01-17 21:40:03 +0000

    Add DevelopmentInProgress.Strategy.Common.Test to solution
    Add DevelopmentInProgress.Strategy.Common.Test to solution

---
+++ UnitTestProject1/TradeCacheTest.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class TradeCacheTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+    }
+}
