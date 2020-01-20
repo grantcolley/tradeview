40d9fba70e5f0dbff739792bb2a9081b4c2a9739
grantcolley <grantcolley@yahoo.com>
2020-01-20 15:23:51 +0000

    Add DevelopmentInProgress.Strategy.Common.Test project
    Add DevelopmentInProgress.Strategy.Common.Test project

---
+++ test/DevelopmentInProgress.Strategy.Common.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DevelopmentInProgress.Strategy.Common.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DevelopmentInProgress.Strategy.Common.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8de2ff1c-be18-4b76-a4c7-feb7e5eaad4d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ test/DevelopmentInProgress.Strategy.Common.Test/TradeCacheTest.cs
@@ -0,0 +1,29 @@
+﻿using DevelopmentInProgress.TradeView.Interface.Model;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DevelopmentInProgress.Strategy.Common.Test
+{
+    [TestClass]
+    public class TradeCacheTest
+    {
+        [TestMethod]
+        public void TradeCache_Initialise()
+        {
+            // Arrange
+            int incrementalSize = 5;
+            TradeCache<Trade> tradeCache;
+
+            // Act
+            tradeCache = new TradeCache<Trade>(incrementalSize);
+
+            // Assert
+            Assert.AreEqual(tradeCache.Position, 0);
+            Assert.AreEqual(tradeCache.CacheSize, 1);
+            Assert.AreEqual(tradeCache.IncrementalSize, 5);
+            Assert.IsNull(tradeCache.GetLastTrade());
+            Assert.AreEqual(tradeCache.GetTrades().Length, 1);
+            Assert.AreEqual(tradeCache.GetLastTrades(1).Length, 1);
+            Assert.AreEqual(tradeCache.GetLastTrades(5).Length, 1);
+        }
+    }
+}
