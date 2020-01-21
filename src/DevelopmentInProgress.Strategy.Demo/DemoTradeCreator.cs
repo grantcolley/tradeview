43e1b8d9d91b9b811cd67ae9f654cf4bca37bed3
grantcolley <grantcolley@yahoo.com>
2020-01-21 20:21:54 +0000

    Create DemoTradeCreator.cs

---
+++ src/DevelopmentInProgress.Strategy.Demo/DemoTradeCreator.cs
@@ -0,0 +1,23 @@
+﻿using DevelopmentInProgress.Strategy.Common;
+using DevelopmentInProgress.TradeView.Interface.Interfaces;
+
+namespace DevelopmentInProgress.Strategy.Demo
+{
+    public class DemoTradeCreator : ITradeCreator<DemoTrade>
+    {
+        public DemoTrade CreateTrade(ITrade trade)
+        {
+            return new DemoTrade
+            {
+                Symbol = trade.Symbol,
+                Exchange = trade.Exchange,
+                Id = trade.Id,
+                Price = trade.Price,
+                Quantity = trade.Quantity,
+                Time = trade.Time,
+                IsBuyerMaker = trade.IsBuyerMaker,
+                IsBestPriceMatch = trade.IsBestPriceMatch
+            };
+        }
+    }
+}
