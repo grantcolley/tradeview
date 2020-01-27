eea2de3fc5211c5172ffb4a748213d634cd4828c
grantcolley <grantcolley@yahoo.com>
2020-01-27 21:00:59 +0000

    MovingAverageTradeParameters to inherit StrategyParameters
    MovingAverageTradeParameters to inherit StrategyParameters

--- src/DevelopmentInProgress.Strategy.Common/Parameter/MovingAverageTradeParameters.cs
+++ src/DevelopmentInProgress.Strategy.Common/Parameter/MovingAverageTradeParameters.cs
@@ -1,7 +1,10 @@
-﻿namespace DevelopmentInProgress.Strategy.Common.Parameter
+﻿using DevelopmentInProgress.TradeView.Interface.Strategy;
+
+namespace DevelopmentInProgress.Strategy.Common.Parameter
 {
-    public class MovingAverageTradeParameters
+    public class MovingAverageTradeParameters : StrategyParameters
     {
+        public int TradeRange { get; set; }
         public decimal BuyIndicator { get; set; }
         public decimal SellIndicator { get; set; }
         public int MovingAvarageRange { get; set; }
