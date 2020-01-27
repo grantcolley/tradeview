b1187ae05c5ece4eac752aca19c7ccc5e089a717
grantcolley <grantcolley@yahoo.com>
2020-01-27 21:00:29 +0000

    Add clause to generic P in ITradeCreator
    Add clause to generic P in ITradeCreator

--- src/DevelopmentInProgress.Strategy.Common/ITradeCreator.cs
+++ src/DevelopmentInProgress.Strategy.Common/ITradeCreator.cs
@@ -1,9 +1,10 @@
 ﻿using DevelopmentInProgress.TradeView.Interface.Interfaces;
-using System.Threading.Tasks;
+using DevelopmentInProgress.TradeView.Interface.Strategy;
 
 namespace DevelopmentInProgress.Strategy.Common
 {
-    public interface ITradeCreator<T, P> where T : ITrade
+    public interface ITradeCreator<T, P>    where T : ITrade
+                                            where P : StrategyParameters
     {
         void Reset(P parameters);
         T CreateTrade(ITrade trade);
