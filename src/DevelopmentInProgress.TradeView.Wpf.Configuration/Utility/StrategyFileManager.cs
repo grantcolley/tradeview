35f4b8066ad8fc414aeabda0bbe77218a9726eee
grantcolley <grantcolley@yahoo.com>
2020-01-28 14:50:18 +0000

    Get class inheriting StrategyParameters from any library
    Get class inheriting StrategyParameters from any library

--- src/DevelopmentInProgress.TradeView.Wpf.Configuration/Utility/StrategyFileManager.cs
+++ src/DevelopmentInProgress.TradeView.Wpf.Configuration/Utility/StrategyFileManager.cs
@@ -3,32 +3,56 @@ using DevelopmentInProgress.TradeView.Wpf.Common.Model;
 using DevelopmentInProgress.TradeView.Wpf.Configuration.View;
 using Newtonsoft.Json;
 using System;
+using System.Collections.Generic;
 using System.Linq;
+using Microsoft.Win32;
 using System.Reflection;
 
 namespace DevelopmentInProgress.TradeView.Wpf.Configuration.Utility
 {
     public class StrategyFileManager : IStrategyFileManager
-    {  
+    {
         public string GetStrategyTypeAsJson(StrategyFile strategyFile)
         {
-            Type type = typeof(StrategyParameters);
-            var assembly = Assembly.LoadFile(strategyFile.File);
-            var types = assembly.GetTypes().Where(t => t.IsSubclassOf(type));
-            if (!types.Any())
+            string file = string.Empty;
+
+            var dialog = new OpenFileDialog
             {
-                throw new Exception($"No types implementing {type.Name} available in {assembly.GetName().Name}");
+                Title = "Select",
+                Multiselect = false,
+                CheckFileExists = true,
+                CheckPathExists = true
+            };
+
+            var result = dialog.ShowDialog();
+            if (result.HasValue
+                && result.Value.Equals(true)
+                && dialog.FileNames != null
+                && dialog.FileNames.Length == 1)
+            {
+                file = dialog.FileNames[0];
             }
 
-            var parameterDialogView = new ParameterDialogView(types);
-            parameterDialogView.ShowDialog();
-
-            if (parameterDialogView.SelectedType != null)
+            if (!string.IsNullOrWhiteSpace(file))
             {
-                var selectedType = parameterDialogView.SelectedType;
+                Type type = typeof(StrategyParameters);
+                var assembly = Assembly.LoadFile(file);
+                var types = assembly.GetTypes().Where(t => t.IsSubclassOf(type));
+                if (!types.Any())
+                {
+                    throw new Exception($"No types implementing {type.Name} available in {assembly.GetName().Name}");
+                }
 
-                var strategyParameters = Activator.CreateInstance(selectedType);
-                return JsonConvert.SerializeObject(strategyParameters, Formatting.Indented);
+                var parameterDialogView = new ParameterDialogView(types);
+                parameterDialogView.ShowDialog();
+
+                if (parameterDialogView.SelectedType != null)
+                {
+                    var selectedType = parameterDialogView.SelectedType;
+
+                    var strategyParameters = Activator.CreateInstance(selectedType);
+                    return JsonConvert.SerializeObject(strategyParameters, Formatting.Indented);
+                }
             }
 
             return string.Empty;
