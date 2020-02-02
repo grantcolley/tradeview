e44aa5345c5ac332797e1c1ae89949cc493562bb
grantcolley <grantcolley@yahoo.com>
2020-02-02 19:54:32 +0000

    Create IStrategyServerService and StrategyServerService
    Create IStrategyServerService and StrategyServerService

---
+++ src/DevelopmentInProgress.TradeView.Wpf.Common/Services/IStrategyServerService.cs
@@ -0,0 +1,14 @@
+﻿using DevelopmentInProgress.TradeView.Wpf.Common.Model;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DevelopmentInProgress.TradeView.Wpf.Common.Services
+{
+    public interface IStrategyServerService
+    {
+        Task<List<StrategyServer>> GetStrategyServers();
+        Task<StrategyServer> GetStrategyServer(string strategyServerName);
+        Task SaveStrategyServer(StrategyServer strategyServer);
+        Task DeleteStrategyServer(StrategyServer strategyServer);
+    }
+}
