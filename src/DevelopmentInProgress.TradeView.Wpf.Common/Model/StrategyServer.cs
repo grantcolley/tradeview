53d5a7b9f25e0d370f7c3279264d8255a4d5a9eb
grantcolley <grantcolley@yahoo.com>
2020-02-02 19:54:09 +0000

    Create wpf StrategyServer and extension methods.
    Create wpf StrategyServer and extension methods.

---
+++ src/DevelopmentInProgress.TradeView.Wpf.Common/Model/StrategyServer.cs
@@ -0,0 +1,34 @@
+﻿namespace DevelopmentInProgress.TradeView.Wpf.Common.Model
+{
+    public class StrategyServer : EntityBase
+    {
+        private string name;
+        private string url;
+
+        public string Name 
+        {
+            get { return name; }
+            set
+            {
+                if(name != value)
+                {
+                    name = value;
+                    OnPropertyChanged("Name");
+                }
+            }
+        }
+
+        public string Url
+        {
+            get { return url; }
+            set
+            {
+                if (url != value)
+                {
+                    url = value;
+                    OnPropertyChanged("Url");
+                }
+            }
+        }
+    }
+}
