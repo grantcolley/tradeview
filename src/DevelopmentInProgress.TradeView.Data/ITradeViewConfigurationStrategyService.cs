d9686dbef25a9d209bbcdc45a2c2eb5d7d3b65d1
grantcolley <grantcolley@yahoo.com>
2020-02-02 19:22:41 +0000

    Create StrategyService and TradeViewConfigurationStrategyServiceFile inheriting ITradeViewConfigurationStrategyService
    Create StrategyService and TradeViewConfigurationStrategyServiceFile inheriting ITradeViewConfigurationStrategyService

---
+++ src/DevelopmentInProgress.TradeView.Data/ITradeViewConfigurationStrategyService.cs
@@ -0,0 +1,14 @@
+﻿using DevelopmentInProgress.TradeView.Interface.Strategy;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DevelopmentInProgress.TradeView.Data
+{
+    public interface ITradeViewConfigurationStrategyService
+    {
+        Task<List<StrategyService>> GetStrategyServicesAsync();
+        Task<StrategyService> GetStrategyServiceAsync(string strategyServiceName);
+        Task SaveStrategyServiceAsync(StrategyService strategyService);
+        Task DeleteStrategyServiceAsync(StrategyService strategyService);
+    }
+}
---
+++ src/DevelopmentInProgress.TradeView.Interface/Strategy/StrategyService.cs
@@ -0,0 +1,8 @@
+﻿namespace DevelopmentInProgress.TradeView.Interface.Strategy
+{
+    public class StrategyService
+    {
+        public string Name { get; set; }
+        public string Url { get; set; }
+    }
+}
