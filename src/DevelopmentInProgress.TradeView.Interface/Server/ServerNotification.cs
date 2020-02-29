5d8b14c310ca0301cfad33e3e6ff10d9b3f7ea0e
grantcolley <grantcolley@yahoo.com>
2020-02-29 08:59:28 +0000

    Create ServerNotification.cs

---
+++ src/DevelopmentInProgress.TradeView.Interface/Server/ServerNotification.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DevelopmentInProgress.TradeView.Interface.Server
+{
+    public class ServerNotification
+    {
+        public string Machine { get; set; }
+        public string Message { get; set; }
+        public string MethodName { get; set; }
+        public DateTime Timestamp { get; set; } = DateTime.Now;
+    }
+}
