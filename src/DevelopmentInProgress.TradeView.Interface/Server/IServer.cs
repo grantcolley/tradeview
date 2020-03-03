7af8cefb928a6509e4a8d232280ec03a3384f6e1
grantcolley <grantcolley@yahoo.com>
2020-03-03 12:51:34 +0000

    Server to implement IServer
    Server to implement IServer

---
+++ src/DevelopmentInProgress.TradeView.Interface/Server/IServer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DevelopmentInProgress.TradeView.Interface.Server
+{
+    public interface IServer
+    {
+        string Name { get; set; }
+        string Url { get; set; }
+        int MaxDegreeOfParallelism { get; set; }
+        string StartedBy { get; set; }
+        string StoppedBy { get; set; }
+        DateTime Started { get; set; }
+        DateTime Stopped { get; set; }
+        List<ServerStrategy> Strategies { get; set; }
+    }
+}
--- src/DevelopmentInProgress.TradeView.Interface/Server/Server.cs
+++ src/DevelopmentInProgress.TradeView.Interface/Server/Server.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 
 namespace DevelopmentInProgress.TradeView.Interface.Server
 {
-    public class Server
+    public class Server : IServer
     {
         public Server()
         {
@@ -13,9 +13,9 @@ namespace DevelopmentInProgress.TradeView.Interface.Server
         public string Name { get; set; }
         public string Url { get; set; }
         public int MaxDegreeOfParallelism { get; set; } = 5;
-        public string StartedBy { get; set; }
+        public string StartedBy { get; set; } = Environment.UserName;
         public string StoppedBy { get; set; }
-        public DateTime Started { get; set; }
+        public DateTime Started { get; set; } = DateTime.Now;
         public DateTime Stopped { get; set; }
         public List<ServerStrategy> Strategies { get; set; }
     }
