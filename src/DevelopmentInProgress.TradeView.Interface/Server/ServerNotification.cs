442e1cfd7b7b808c7083ac059ad13b3383fba067
grantcolley <grantcolley@yahoo.com>
2020-03-05 19:56:13 +0000

    Update ServerNotification.cs

--- src/DevelopmentInProgress.TradeView.Interface/Server/ServerNotification.cs
+++ src/DevelopmentInProgress.TradeView.Interface/Server/ServerNotification.cs
@@ -6,7 +6,6 @@ namespace DevelopmentInProgress.TradeView.Interface.Server
     {
         public string Machine { get; set; }
         public string Message { get; set; }
-        public string MethodName { get; set; }
         public DateTime Timestamp { get; set; } = DateTime.Now;
     }
 }
