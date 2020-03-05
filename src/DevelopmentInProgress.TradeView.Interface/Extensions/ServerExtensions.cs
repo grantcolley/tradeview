e3617284323bd57c19e6bb295b5d00cbf821e18e
grantcolley <grantcolley@yahoo.com>
2020-03-05 19:57:11 +0000

    Create ServerExtensions.cs

---
+++ src/DevelopmentInProgress.TradeView.Interface/Extensions/ServerExtensions.cs
@@ -0,0 +1,16 @@
+﻿using DevelopmentInProgress.TradeView.Interface.Server;
+using System;
+
+namespace DevelopmentInProgress.TradeView.Interface.Extensions
+{
+    public static class ServerExtensions
+    {
+        public static ServerNotification GetNotification(this Server.Server server)
+        {
+            return new ServerNotification
+            {
+                 Machine = Environment.MachineName
+            };
+        }
+    }
+}
