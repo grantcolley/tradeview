0ef03df8092a05bbcf76692fdef51e428f1c7a42
grantcolley <grantcolley@yahoo.com>
2020-03-08 18:47:02 +0000

    Create IDashboardService and DashboardService and add to config
    Create IDashboardService and DashboardService and add to config

---
+++ src/DevelopmentInProgress.TradeView.Wpf.Dashboard/Services/IDashboardService.cs
@@ -0,0 +1,11 @@
+﻿using DevelopmentInProgress.TradeView.Wpf.Dashboard.Model;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DevelopmentInProgress.TradeView.Wpf.Dashboard.Services
+{
+    public interface IDashboardService
+    {
+        Task<List<ServerMonitor>> GetServers();
+    }
+}
