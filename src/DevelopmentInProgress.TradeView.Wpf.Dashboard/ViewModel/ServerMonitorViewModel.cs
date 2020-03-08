b537b67650160e468330854ca4dcb9aece9538ac
grantcolley <grantcolley@yahoo.com>
2020-03-08 09:44:35 +0000

    Update ServerMonitorViewModel.cs

--- src/DevelopmentInProgress.TradeView.Wpf.Dashboard/ViewModel/ServerMonitorViewModel.cs
+++ src/DevelopmentInProgress.TradeView.Wpf.Dashboard/ViewModel/ServerMonitorViewModel.cs
@@ -1,18 +1,53 @@
-﻿using DevelopmentInProgress.TradeView.Wpf.Host.Context;
+﻿using DevelopmentInProgress.TradeView.Wpf.Common.Model;
+using DevelopmentInProgress.TradeView.Wpf.Common.Services;
+using DevelopmentInProgress.TradeView.Wpf.Host.Context;
 using DevelopmentInProgress.TradeView.Wpf.Host.ViewModel;
+using System.Collections.Generic;
 
 namespace DevelopmentInProgress.TradeView.Wpf.Dashboard.ViewModel
 {
     public class ServerMonitorViewModel : DocumentViewModel
     {
-        public ServerMonitorViewModel(ViewModelContext viewModelContext)
+        private Server server;
+        private List<Server> servers;
+        private readonly IServerService serverService;
+
+        public ServerMonitorViewModel(ViewModelContext viewModelContext, IServerService serverService)
             : base(viewModelContext)
         {
+            this.serverService = serverService;
+        }
+
+        public List<Server> Servers
+        {
+            get { return servers; }
+            set
+            {
+                if(servers != value)
+                {
+                    servers = value;
+                    OnPropertyChanged("Servers");
+                }
+            }
+        }
+
+        public Server Server
+        {
+            get { return server; }
+            set
+            {
+                if (server != value)
+                {
+                    server = value;
+                    OnPropertyChanged("Server");
+                }
+            }
+
         }
 
         protected async override void OnPublished(object data)
         {
-            // Do stuff here...
+            Servers = await serverService.GetServers();
         }
     }
 }
