e2483db18b777c4044df8d795b10a43c8bb148de
grantcolley <grantcolley@yahoo.com>
2020-03-08 18:46:22 +0000

    Create ServerMonitorExtensions.cs

---
+++ src/DevelopmentInProgress.TradeView.Wpf.Dashboard/Extensions/ServerMonitorExtensions.cs
@@ -0,0 +1,17 @@
+﻿using DevelopmentInProgress.TradeView.Wpf.Dashboard.Model;
+
+namespace DevelopmentInProgress.TradeView.Wpf.Dashboard.Extensions
+{
+    public static class ServerMonitorExtensions
+    {
+        public static ServerMonitor ToServerMonitor(this Interface.Server.Server server)
+        {
+            return new ServerMonitor
+            {
+                Name = server.Name,
+                Url = server.Url,
+                MaxDegreeOfParallelism = server.MaxDegreeOfParallelism
+            };
+        }
+    }
+}
