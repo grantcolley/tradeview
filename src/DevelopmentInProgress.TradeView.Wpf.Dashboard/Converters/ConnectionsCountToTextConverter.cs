5b3e9366c6086caea904a47d5487ee58f41908c3
grantcolley <grantcolley@yahoo.com>
2020-03-10 19:28:48 +0000

    Add dashboard xaml converters
    Add dashboard xaml converters

---
+++ src/DevelopmentInProgress.TradeView.Wpf.Dashboard/Converters/ConnectionsCountToTextConverter.cs
@@ -0,0 +1,28 @@
+﻿using DevelopmentInProgress.TradeView.Wpf.Dashboard.Model;
+using System;
+using System.Collections.ObjectModel;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace DevelopmentInProgress.TradeView.Wpf.Dashboard.Converters
+{
+    public class ConnectionsCountToTextConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var connections = value as ObservableCollection<Connection>;
+            if (value == null)
+            {
+                return "Connections (0)";
+            }
+
+            return $"Connections ({connections.Count})";
+        }
+
+        public object ConvertBack(object value, Type targetType,
+                                  object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/DevelopmentInProgress.TradeView.Wpf.Dashboard/Converters/StrategiesCountToTextConverter.cs
@@ -0,0 +1,28 @@
+﻿using DevelopmentInProgress.TradeView.Wpf.Dashboard.Model;
+using System;
+using System.Collections.ObjectModel;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace DevelopmentInProgress.TradeView.Wpf.Dashboard.Converters
+{
+    public class StrategiesCountToTextConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var serverStrategies = value as ObservableCollection<ServerStrategy>;
+            if (value == null)
+            {
+                return "Strategies (0)";
+            }
+
+            return $"Strategies ({serverStrategies.Count})";
+        }
+
+        public object ConvertBack(object value, Type targetType,
+                                  object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
