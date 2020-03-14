16ecca793898ea8c5e504236c94752857cab6edd
grantcolley <grantcolley@yahoo.com>
2020-03-14 17:43:51 +0000

    Create ServerMonitorEventArgs.cs

---
+++ src/DevelopmentInProgress.TradeView.Wpf.Dashboard/Events/ServerMonitorEventArgs.cs
@@ -0,0 +1,9 @@
+﻿using DevelopmentInProgress.TradeView.Wpf.Common.Events;
+using DevelopmentInProgress.TradeView.Wpf.Dashboard.Model;
+
+namespace DevelopmentInProgress.TradeView.Wpf.Dashboard.Events
+{
+    public class ServerMonitorEventArgs : EventArgsBase<ServerMonitor>
+    {
+    }
+}
