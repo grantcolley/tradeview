d26a37b617ddb101144d015ed89f67d34c39db7b
grantcolley <grantcolley@yahoo.com>
2020-03-15 21:19:42 +0000

    Create ServerNotificationLevel.cs

---
+++ src/DevelopmentInProgress.TradeView.Interface/Server/ServerNotificationLevel.cs
@@ -0,0 +1,11 @@
+﻿namespace DevelopmentInProgress.TradeView.Interface.Server
+{
+    public enum ServerNotificationLevel
+    {
+        Debug = 0,
+        Information = 1,
+        Warning = 2,
+        Error = 3,
+        DisconnectClient = 4
+    }
+}
