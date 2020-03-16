60226ecf3da7f092ee6e930c24670ea30595057d
grantcolley <grantcolley@yahoo.com>
2020-03-16 21:17:52 +0000

    Update TradeViewConfigurationServerFile.cs

--- src/DevelopmentInProgress.TradeView.Data.File/TradeViewConfigurationServerFile.cs
+++ src/DevelopmentInProgress.TradeView.Data.File/TradeViewConfigurationServerFile.cs
@@ -126,7 +126,7 @@ namespace DevelopmentInProgress.TradeView.Data.File
         {
             return new Server
             {
-                Name = "Server #1",
+                Name = "TradeServer",
                 Url = "http://localhost:5500",
                 MaxDegreeOfParallelism = 5
             };
