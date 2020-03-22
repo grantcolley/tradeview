a155b37bad4c6b1b531e6cc8b6f02f54d8172573
grantcolley <grantcolley@yahoo.com>
2020-03-22 12:46:52 +0000

    Update Connection.cs

--- src/DevelopmentInProgress.TradeView.Wpf.Dashboard/Model/Connection.cs
+++ src/DevelopmentInProgress.TradeView.Wpf.Dashboard/Model/Connection.cs
@@ -1,10 +1,12 @@
 ﻿using DevelopmentInProgress.TradeView.Wpf.Common.Model;
+using System;
 
 namespace DevelopmentInProgress.TradeView.Wpf.Dashboard.Model
 {
     public class Connection : EntityBase
     {
         private string name;
+        private DateTime connected;
 
         public string Name
         {
@@ -15,6 +17,19 @@ namespace DevelopmentInProgress.TradeView.Wpf.Dashboard.Model
                 {
                     name = value;
                     OnPropertyChanged("Name");
+                }
+            }
+        }
+
+        public DateTime Connected
+        {
+            get { return connected; }
+            set
+            {
+                if (connected != value)
+                {
+                    connected = value;
+                    OnPropertyChanged("Connected");
                 }
             }
         }
