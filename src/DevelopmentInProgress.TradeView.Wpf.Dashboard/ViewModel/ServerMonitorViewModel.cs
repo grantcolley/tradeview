4422aeb1ac75da82257963a5c6258975f023ed8c
grantcolley <grantcolley@yahoo.com>
2020-03-24 18:58:27 +0000

    Update ServerMonitorViewModel.cs

--- src/DevelopmentInProgress.TradeView.Wpf.Dashboard/ViewModel/ServerMonitorViewModel.cs
+++ src/DevelopmentInProgress.TradeView.Wpf.Dashboard/ViewModel/ServerMonitorViewModel.cs
@@ -10,7 +10,6 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Reactive.Linq;
-using System.Threading;
 using System.Threading.Tasks;
 
 namespace DevelopmentInProgress.TradeView.Wpf.Dashboard.ViewModel
