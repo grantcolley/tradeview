86b1d881ddb65273a1db555ef6435055d21ba99f
grantcolley <grantcolley@yahoo.com>
2020-03-27 18:15:02 +0000

    Remove ServerMonitor and related classes from Dashboard
    Remove ServerMonitor and related classes from Dashboard

--- src/DevelopmentInProgress.TradeView.Wpf.Dashboard/Events/ServerMonitorEventArgs.cs
+++
@@ -1,9 +0,0 @@
-﻿using DevelopmentInProgress.TradeView.Wpf.Common.Events;
-using DevelopmentInProgress.TradeView.Wpf.Dashboard.Model;
-
-namespace DevelopmentInProgress.TradeView.Wpf.Dashboard.Events
-{
-    public class ServerMonitorEventArgs : EventArgsBase<ServerMonitor>
-    {
-    }
-}
