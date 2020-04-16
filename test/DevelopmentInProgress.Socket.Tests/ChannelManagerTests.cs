d717ef91bb2afcb1a0ac878c6c05f1507dbe7543
grantcolley <grantcolley@yahoo.com>
2020-04-16 19:57:19 +0100

    Add soecket test  project

---
+++ test/DevelopmentInProgress.Socket.Tests/ChannelManagerTests.cs
@@ -0,0 +1,205 @@
+﻿using DevelopmentInProgress.Socket.Messages;
+using DevelopmentInProgress.Socket.Server;
+using System.Net.WebSockets;
+using Xunit;
+
+namespace DevelopmentInProgress.Socket.Tests
+{
+    public class ChannelManagerTests
+    {
+        [Fact]
+        public void SubscribeToChannel()
+        {
+            // Arrange
+            var channelManager = new ChannelManager();
+            var connection1 = new Connection(new ClientWebSocket()) { ConnectionId = "123", Name = "conn1" };
+            var connection2 = new Connection(new ClientWebSocket()) { ConnectionId = "456", Name = "conn2" };
+
+            // Act
+            var channelResult1 = channelManager.SubscribeToChannel("channel1", connection1);
+            var channelResult2 = channelManager.SubscribeToChannel("channel1", connection2);
+
+            // Assert
+            Assert.Equal(channelResult1, channelResult2);
+            Assert.Equal("channel1", channelResult1.Name);
+            Assert.Equal(2, channelResult1.Connections.Count);
+            Assert.Contains(connection1.ConnectionId, channelResult1.Connections.Keys);
+            Assert.Contains(connection2.ConnectionId, channelResult1.Connections.Keys);
+            Assert.Contains(channelResult1.Name, connection1.Channels.Keys);
+            Assert.Contains(channelResult2.Name, connection2.Channels.Keys);
+        }
+
+        [Fact]
+        public void GetChannel()
+        {
+            // Arrange
+            var channelManager = new ChannelManager();
+            var connection = new Connection(new ClientWebSocket()) { ConnectionId = "123", Name = "conn" };
+            var channel = channelManager.SubscribeToChannel("channel", connection);
+
+            // Act
+            var result = channelManager.GetChannel(channel.Name);
+
+            // Assert
+            Assert.Equal(channel, result);
+        }
+
+        [Fact]
+        public void GetChannel_NotFound()
+        {
+            // Arrange
+            var channelManager = new ChannelManager();
+            var connection = new Connection(new ClientWebSocket()) { ConnectionId = "123", Name = "conn" };
+            var channel = channelManager.SubscribeToChannel("channel", connection);
+
+            // Act
+            var result = channelManager.GetChannel("ABC");
+
+            // Assert
+            Assert.Null(result);
+        }
+
+        [Fact]
+        public void GetChannels()
+        {
+            // Arrange
+            var channelManager = new ChannelManager();
+            var connection1 = new Connection(new ClientWebSocket()) { ConnectionId = "123", Name = "conn1" };
+            var connection2 = new Connection(new ClientWebSocket()) { ConnectionId = "456", Name = "conn2" };
+            var channel1 = channelManager.SubscribeToChannel("channel1", connection1);
+            var channel2 = channelManager.SubscribeToChannel("channel2", connection2);
+
+            // Act
+            var channels = channelManager.GetChannels();
+
+            // Assert
+            Assert.Equal(2, channels.Count);
+            Assert.Contains(channel1, channels);
+            Assert.Contains(channel2, channels);
+        }
+
+        [Fact]
+        public void GetChannelInfos()
+        {
+            // Arrange
+            var channelManager = new ChannelManager();
+            var connection1 = new Connection(new ClientWebSocket()) { ConnectionId = "123", Name = "conn1" };
+            var connection2 = new Connection(new ClientWebSocket()) { ConnectionId = "456", Name = "conn2" };
+            var channel1 = channelManager.SubscribeToChannel("channel1", connection1);
+            var channel2 = channelManager.SubscribeToChannel("channel2", connection2);
+
+            // Act
+            var channelInfos = channelManager.GetChannelInfos();
+
+            // Assert
+            Assert.Equal(2, channelInfos.Count);
+            Assert.Single<ChannelInfo>(channelInfos, ci => { return ci.Name.Equals("channel1"); });
+            Assert.Single<ChannelInfo>(channelInfos, ci => { return ci.Name.Equals("channel2"); });
+        }
+
+        [Fact]
+        public void TryRemoveChannel_NotFound()
+        {
+            // Arrange
+            var channelManager = new ChannelManager();
+            var connection = new Connection(new ClientWebSocket()) { ConnectionId = "123", Name = "conn" };
+            var channel = channelManager.SubscribeToChannel("channel", connection);
+
+            // Act
+            var result = channelManager.TryRemoveChannel("ABC", out Channel notFound);
+
+            // Assert
+            Assert.False(result);
+            Assert.Null(notFound);
+        }
+
+        [Fact]
+        public void TryRemoveChannel()
+        {
+            // Arrange
+            var channelManager = new ChannelManager();
+            var connection1 = new Connection(new ClientWebSocket()) { ConnectionId = "123", Name = "conn1" };
+            var connection2 = new Connection(new ClientWebSocket()) { ConnectionId = "456", Name = "conn2" };
+            var channelResult1 = channelManager.SubscribeToChannel("channel1", connection1);
+            var channelResult2 = channelManager.SubscribeToChannel("channel1", connection2);
+
+            // Act
+            var result = channelManager.TryRemoveChannel("channel1", out Channel channel);
+
+            // Assert
+            Assert.True(result);
+            Assert.Equal("channel1", channel.Name);
+            Assert.Empty(channel.Connections);
+            Assert.Empty(connection1.Channels);
+            Assert.Empty(connection2.Channels);
+        }
+
+        [Fact]
+        public void UnsubscribeFromChannel_NotFound()
+        {
+            // Arrange
+            var channelManager = new ChannelManager();
+            var connection1 = new Connection(new ClientWebSocket()) { ConnectionId = "123", Name = "conn1" };
+            var connection2 = new Connection(new ClientWebSocket()) { ConnectionId = "456", Name = "conn2" };
+            var channelResult1 = channelManager.SubscribeToChannel("channel1", connection1);
+            var channelResult2 = channelManager.SubscribeToChannel("channel1", connection2);
+
+            // Act
+            var result = channelManager.UnsubscribeFromChannel("ABC", connection1);
+
+            // Assert
+            Assert.Null(result);
+
+            var channel = channelManager.GetChannel("channel1");
+            Assert.Equal(channelResult1, channel);
+            Assert.Contains(connection1.ConnectionId, channel.Connections.Keys);
+        }
+
+        [Fact]
+        public void UnsubscribeFromChannel_SingleUser()
+        {
+            // Arrange
+            var channelManager = new ChannelManager();
+            var connection1 = new Connection(new ClientWebSocket()) { ConnectionId = "123", Name = "conn1" };
+            var connection2 = new Connection(new ClientWebSocket()) { ConnectionId = "456", Name = "conn2" };
+            var channelResult1 = channelManager.SubscribeToChannel("channel1", connection1);
+            var channelResult2 = channelManager.SubscribeToChannel("channel1", connection2);
+
+            // Act
+            var result = channelManager.UnsubscribeFromChannel("channel1", connection1);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(channelResult1, result);
+            Assert.Contains(connection2.ConnectionId, result.Connections.Keys);
+            Assert.DoesNotContain(connection1.ConnectionId, result.Connections.Keys);
+            Assert.DoesNotContain(result.Name, connection1.Channels.Keys);
+        }
+
+        [Fact]
+        public void UnsubscribeFromChannel_AllUsers()
+        {
+            // Arrange
+            var channelManager = new ChannelManager();
+            var connection1 = new Connection(new ClientWebSocket()) { ConnectionId = "123", Name = "conn1" };
+            var connection2 = new Connection(new ClientWebSocket()) { ConnectionId = "456", Name = "conn2" };
+            var channelResult1 = channelManager.SubscribeToChannel("channel1", connection1);
+            var channelResult2 = channelManager.SubscribeToChannel("channel1", connection2);
+
+            // Act
+            var result1 = channelManager.UnsubscribeFromChannel("channel1", connection1);
+            var result2 = channelManager.UnsubscribeFromChannel("channel1", connection2);
+
+            // Assert
+            Assert.NotNull(result1);
+            Assert.NotNull(result2);
+            Assert.Equal(channelResult1, result1);
+            Assert.Equal(channelResult1, result2);
+            Assert.DoesNotContain(connection1.ConnectionId, channelResult1.Connections.Keys);
+            Assert.DoesNotContain(connection2.ConnectionId, channelResult1.Connections.Keys);
+            Assert.Empty(channelResult1.Connections);
+            Assert.DoesNotContain(channelResult1.Name, connection1.Channels.Keys);
+            Assert.DoesNotContain(channelResult1.Name, connection2.Channels.Keys);
+        }
+    }
+}
---
+++ test/DevelopmentInProgress.Socket.Tests/ConnectionManagerTests.cs
@@ -0,0 +1,176 @@
+using DevelopmentInProgress.Socket.Messages;
+using DevelopmentInProgress.Socket.Server;
+using System.Linq;
+using System.Net.WebSockets;
+using Xunit;
+
+namespace DevelopmentInProgress.Socket.Tests
+{
+    public class ConnectionManagerTests
+    {
+        [Fact]
+        public void TryAddWebSocketConnection()
+        {
+            // Arrange
+            var connectionManager = new ConnectionManager();
+            var webSocket = new ClientWebSocket();
+
+            // Act
+            var result = connectionManager.TryAddWebSocketConnection(webSocket, out Connection connection);
+
+            // Assert
+            Assert.True(result);
+            Assert.NotNull(connection);
+        }
+
+        [Fact]
+        public void GetConnectionId()
+        {
+            // Arrange
+            var connectionManager = new ConnectionManager();
+            var webSocket = new ClientWebSocket();
+            var result = connectionManager.TryAddWebSocketConnection(webSocket, out Connection connection);
+
+            // Act
+            var connectionId = connectionManager.GetConnectionId(webSocket);
+
+            // Assert
+            Assert.Equal(connection.ConnectionId, connectionId);
+        }
+
+        [Fact]
+        public void GetConnectionId_NotFound()
+        {
+            // Arrange
+            var connectionManager = new ConnectionManager();
+            var webSocket1 = new ClientWebSocket();
+            var webSocket2 = new ClientWebSocket();
+            var result = connectionManager.TryAddWebSocketConnection(webSocket1, out Connection connection);
+
+            // Act
+            var connectionId = connectionManager.GetConnectionId(webSocket2);
+
+            // Assert
+            Assert.Null(connectionId);
+        }
+
+        [Fact]
+        public void GetConnection_WebSocket()
+        {
+            // Arrange
+            var connectionManager = new ConnectionManager();
+            var webSocket = new ClientWebSocket();
+            var result = connectionManager.TryAddWebSocketConnection(webSocket, out Connection connection);
+
+            // Act
+            var conn = connectionManager.GetConnection(webSocket);
+
+            // Assert
+            Assert.Equal(connection, conn);
+        }
+
+        [Fact]
+        public void GetConnection_WebSocket_NotFound()
+        {
+            // Arrange
+            var connectionManager = new ConnectionManager();
+            var webSocket1 = new ClientWebSocket();
+            var webSocket2 = new ClientWebSocket();
+            var result = connectionManager.TryAddWebSocketConnection(webSocket1, out Connection connection);
+
+            // Act
+            var conn = connectionManager.GetConnection(webSocket2);
+
+            // Assert
+            Assert.Null(conn);
+        }
+
+        [Fact]
+        public void GetConnection_ConnectionId()
+        {
+            // Arrange
+            var connectionManager = new ConnectionManager();
+            var webSocket = new ClientWebSocket();
+            var result = connectionManager.TryAddWebSocketConnection(webSocket, out Connection connection);
+
+            // Act
+            var conn = connectionManager.GetConnection(connection.ConnectionId);
+
+            // Assert
+            Assert.Equal(connection, conn);
+        }
+
+        [Fact]
+        public void GetConnection_ConnectionId_NotFound()
+        {
+            // Arrange
+            var connectionManager = new ConnectionManager();
+            var webSocket = new ClientWebSocket();
+            var result = connectionManager.TryAddWebSocketConnection(webSocket, out Connection connection);
+
+            // Act
+            var conn = connectionManager.GetConnection("123");
+
+            // Assert
+            Assert.Null(conn);
+        }
+
+        [Fact]
+        public void GetConnections()
+        {
+            // Arrange
+            var connectionManager = new ConnectionManager();
+            var webSocket1 = new ClientWebSocket();
+            var webSocket2 = new ClientWebSocket();
+            var result1 = connectionManager.TryAddWebSocketConnection(webSocket1, out Connection connection1);
+            var result2 = connectionManager.TryAddWebSocketConnection(webSocket2, out Connection connection2);
+
+            // Act
+            var connections = connectionManager.GetConnections();
+
+            // Assert
+            Assert.Equal(2, connections.Count());
+            Assert.Contains(connection1, connections);
+            Assert.Contains(connection2, connections);
+        }
+
+        [Fact]
+        public void GetConnectionInfos()
+        {
+            // Arrange
+            var connectionManager = new ConnectionManager();
+            var webSocket1 = new ClientWebSocket();
+            var webSocket2 = new ClientWebSocket();
+            var result1 = connectionManager.TryAddWebSocketConnection(webSocket1, out Connection connection1);
+            var result2 = connectionManager.TryAddWebSocketConnection(webSocket2, out Connection connection2);
+
+            // Act
+            var connectionInfos = connectionManager.GetConnectionInfos();
+
+            // Assert
+            Assert.Equal(2, connectionInfos.Count());
+            Assert.Single(connectionInfos.Where(ci => ci.ConnectionId.Equals(connection1.GetConnectionInfo().ConnectionId)));
+            Assert.Single(connectionInfos.Where(ci => ci.ConnectionId.Equals(connection2.GetConnectionInfo().ConnectionId)));
+        }
+
+        [Fact]
+        public void TryRemoveWebSocketConnection()
+        {
+            // Arrange
+            var connectionManager = new ConnectionManager();
+            var webSocket1 = new ClientWebSocket();
+            var webSocket2 = new ClientWebSocket();
+            var result1 = connectionManager.TryAddWebSocketConnection(webSocket1, out Connection connection1);
+            var result2 = connectionManager.TryAddWebSocketConnection(webSocket2, out Connection connection2);
+
+            // Act
+            var result = connectionManager.TryRemoveWebSocketConnection(webSocket1, out Connection connection);
+
+            // Assert
+            var connections = connectionManager.GetConnections();
+            Assert.Single(connections);
+            Assert.Equal(connection2, connections[0]);
+            Assert.Equal(connection1, connection);
+        }
+    }
+}
