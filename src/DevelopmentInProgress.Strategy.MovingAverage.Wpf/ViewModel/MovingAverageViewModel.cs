15124a73a2f9aa2ae6e063b361e236334db7952e
grantcolley <grantcolley@yahoo.com>
2020-07-25 19:00:25 +0100

    IDE0062 C# Local function can be made static
    IDE0062 C# Local function can be made static

--- src/DevelopmentInProgress.Strategy.MovingAverage.Wpf/ViewModel/MovingAverageViewModel.cs
+++ src/DevelopmentInProgress.Strategy.MovingAverage.Wpf/ViewModel/MovingAverageViewModel.cs
@@ -200,9 +200,9 @@ namespace DevelopmentInProgress.Strategy.MovingAverage.Wpf.ViewModel
 
                     var tradeHelper = tradeHelperFactory.GetTradeHelper(trade.Exchange);
 
-                    Trade createSmaTrade(ITrade t, int p, int q) => new Trade { Price = ((MovingAverageTrade)t).MovingAveragePrice.Trim(p), Time = t.Time.ToLocalTime(), Exchange = t.Exchange };
-                    Trade createBuyIndicator(ITrade t, int p, int q) => new Trade { Price = ((MovingAverageTrade)t).BuyPrice.Trim(p), Time = t.Time.ToLocalTime(), Exchange = t.Exchange };
-                    Trade createSellIndicator(ITrade t, int p, int q) => new Trade { Price = ((MovingAverageTrade)t).SellPrice.Trim(p), Time = t.Time.ToLocalTime(), Exchange = t.Exchange };
+                    static Trade createSmaTrade(ITrade t, int p, int q) => new Trade { Price = ((MovingAverageTrade)t).MovingAveragePrice.Trim(p), Time = t.Time.ToLocalTime(), Exchange = t.Exchange };
+                    static Trade createBuyIndicator(ITrade t, int p, int q) => new Trade { Price = ((MovingAverageTrade)t).BuyPrice.Trim(p), Time = t.Time.ToLocalTime(), Exchange = t.Exchange };
+                    static Trade createSellIndicator(ITrade t, int p, int q) => new Trade { Price = ((MovingAverageTrade)t).SellPrice.Trim(p), Time = t.Time.ToLocalTime(), Exchange = t.Exchange };
 
                     var tradesDisplayCount = Strategy.TradesDisplayCount;
                     var tradesChartDisplayCount = Strategy.TradesChartDisplayCount;
