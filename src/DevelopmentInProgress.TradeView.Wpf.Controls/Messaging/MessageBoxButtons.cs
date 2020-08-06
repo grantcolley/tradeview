04886072547542c432b55a8f3b084953adb9051b
grantcolley <grantcolley@yahoo.com>
2020-08-06 18:31:55 +0100

    CA1717:Only FlagsAttribute enums should have plural names
    CA1717:Only FlagsAttribute enums should have plural names

--- src/DevelopmentInProgress.TradeView.Wpf.Controls/Messaging/MessageBoxButtons.cs
+++ src/DevelopmentInProgress.TradeView.Wpf.Controls/Messaging/MessageBoxButtons.cs
@@ -9,6 +9,7 @@ namespace DevelopmentInProgress.TradeView.Wpf.Controls.Messaging
     /// <summary>
     /// The buttons to be displayed on the mesage box.
     /// </summary>
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Naming", "CA1717:Only FlagsAttribute enums should have plural names")]
     public enum MessageBoxButtons
     {
         Ok,
