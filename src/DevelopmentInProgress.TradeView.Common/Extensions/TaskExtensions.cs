f5c328067d375bc1eb98d1d9e5711e6e77b0764d
grantcolley <grantcolley@yahoo.com>
2020-08-15 20:52:34 +0100

    Add Microsoft.CodeAnalysis.FxCopAnalyzers to TradeView.Common.csproj
    CA1062: Validate arguments of public methods
    Supress CA1031:Do not catch general exception types
    Supress CA1030:Use events where appropriate

--- src/DevelopmentInProgress.TradeView.Common/Extensions/TaskExtensions.cs
+++ src/DevelopmentInProgress.TradeView.Common/Extensions/TaskExtensions.cs
@@ -9,8 +9,15 @@ namespace DevelopmentInProgress.TradeView.Common.Extensions
         /// Warning: Exceptions will be swallowed. The function being executed must deal with exceptions.
         /// </summary>
         /// <param name="task">The task to 'fire and forget'</param>
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CA1031:Do not catch general exception types")]
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CA1030:Use events where appropriate")]
         public static async void FireAndForget(this Task task)
         {
+            if(task == null)
+            {
+                throw new ArgumentNullException(nameof(task));
+            }
+
             try
             {
                 await task.ConfigureAwait(false);
