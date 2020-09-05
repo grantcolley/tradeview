79251e17018644bbc8084001e575a80143ed3815
grantcolley <grantcolley@yahoo.com>
2020-09-05 21:05:59 +0100

    Add HttpClientManager and IHttpClientManager
    Add HttpClientManager and IHttpClientManager

---
+++ src/DevelopmentInProgress.TradeView.Wpf.Common/Manager/HttpClientManager.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Net.Http;
+
+namespace DevelopmentInProgress.TradeView.Wpf.Common.Manager
+{
+    public sealed class HttpClientManager : IHttpClientManager
+    {
+        private HttpClientHandler httpClientHandler;
+        private HttpClient httpClient;
+        private bool disposed;
+
+        public HttpClientManager()
+        {
+            httpClientHandler = new HttpClientHandler();
+            httpClient = new HttpClient(httpClientHandler);
+        }
+
+        public HttpClient HttpClientInstance
+        {
+            get { return httpClient; }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        private void Dispose(bool disposing)
+        {
+            if(disposed)
+            {
+                return;
+            }
+
+            if(disposing)
+            {
+                httpClientHandler.Dispose();
+                httpClient.Dispose();
+                httpClientHandler = null;
+                httpClient = null;
+            }
+
+            disposed = true;
+        }
+    }
+}
---
+++ src/DevelopmentInProgress.TradeView.Wpf.Common/Manager/IHttpClientManager.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Net.Http;
+
+namespace DevelopmentInProgress.TradeView.Wpf.Common.Manager
+{
+    public interface IHttpClientManager : IDisposable
+    {
+        HttpClient HttpClientInstance { get; }
+    }
+}
