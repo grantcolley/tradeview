723211a26e7c0b2aea7581d68c35d604c8b86b9f
grantcolley <grantcolley@yahoo.com>
2020-09-26 21:18:39 +0100

    Change ShowFavourites to use a Command instead of a property
    Change ShowFavourites to use a Command instead of a property

--- src/DevelopmentInProgress.TradeView.Wpf.Configuration/ViewModel/SymbolsViewModel.cs
+++ src/DevelopmentInProgress.TradeView.Wpf.Configuration/ViewModel/SymbolsViewModel.cs
@@ -8,8 +8,10 @@ using Prism.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Input;
 
 namespace DevelopmentInProgress.TradeView.Wpf.Configuration.ViewModel
@@ -17,7 +19,6 @@ namespace DevelopmentInProgress.TradeView.Wpf.Configuration.ViewModel
     public class SymbolsViewModel : ExchangeViewModel
     {
         private readonly UserAccount userAccount;
-        private bool showFavourites;
         private bool isLoadingSymbols;
         private bool disposed;
 
@@ -27,6 +28,7 @@ namespace DevelopmentInProgress.TradeView.Wpf.Configuration.ViewModel
             this.userAccount = userAccount;
 
             UpdatePreferencesCommand = new ViewModelCommand(UpdatePreferences);
+            ShowFavouritesCommand = new ViewModelCommand(ShowFavourites);
 
             Symbols = new List<Symbol>();
 
@@ -36,34 +38,13 @@ namespace DevelopmentInProgress.TradeView.Wpf.Configuration.ViewModel
         }
 
         public ICommand UpdatePreferencesCommand { get; set; }
+        public ICommand ShowFavouritesCommand { get; set; }
 
         public List<Symbol> Symbols { get; }
 
         public string Exchange
         {
             get { return userAccount.Exchange.ToString(); }
-        }
-
-        public bool ShowFavourites
-        {
-            get { return showFavourites; }
-            set
-            {
-                if (showFavourites != value)
-                {
-                    showFavourites = value;
-                    if (showFavourites)
-                    {
-                        Symbols.ForEach(s => s.IsVisible = s.IsFavourite);
-                    }
-                    else
-                    {
-                        Symbols.ForEach(s => s.IsVisible = true);
-                    }
-
-                    OnPropertyChanged(nameof(ShowFavourites));
-                }
-            }
         }
 
         public bool IsLoadingSymbols
@@ -144,6 +125,44 @@ namespace DevelopmentInProgress.TradeView.Wpf.Configuration.ViewModel
                     }
                 }
             }
+        }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CA1031:Do not catch general exception types", Justification = "General exceptions are logged and displayed to the user in a dialog box.")]
+        private async void ShowFavourites(object arg)
+        {
+            if (arg != null && arg is bool showFavourites)
+            {
+                IsLoadingSymbols = true;
+
+                try
+                {
+                    await OnShowFavourites(showFavourites).ConfigureAwait(true);
+                }
+                catch (Exception ex)
+                {
+                    Logger.Log(ex.ToString(), Category.Exception, Priority.Low);
+                    Dialog.ShowException(ex);
+                }
+                finally
+                {
+                    IsLoadingSymbols = false;
+                }
+            }
+        }
+
+        private Task OnShowFavourites(bool showFavourites)
+        {
+            return Task.Run(() =>
+            {
+                if (showFavourites)
+                {
+                    Symbols.ForEach(s => s.IsVisible = s.IsFavourite);
+                }
+                else
+                {
+                    Symbols.ForEach(s => s.IsVisible = true);
+                }
+            });
         }
     }
 }
