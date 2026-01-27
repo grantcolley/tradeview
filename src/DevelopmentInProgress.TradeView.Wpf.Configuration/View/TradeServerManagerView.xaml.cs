127e3d0b72377161319ec221d8ade11f71f5a49e
Grant Colley <grantcolley@yahoo.com>
2026-01-27 21:15:21 +0000

    Update TradeServerManagerView.xaml.cs

--- src/DevelopmentInProgress.TradeView.Wpf.Configuration/View/TradeServerManagerView.xaml.cs
+++ src/DevelopmentInProgress.TradeView.Wpf.Configuration/View/TradeServerManagerView.xaml.cs
@@ -5,16 +5,16 @@ using DevelopmentInProgress.TradeView.Wpf.Host.Controller.View;
 namespace DevelopmentInProgress.TradeView.Wpf.Configuration.View
 {
     /// <summary>
-    /// Interaction logic for ServerManagerView.xaml
+    /// Interaction logic for TradeServerManagerView.xaml
     /// </summary>
     public partial class TradeServerManagerView : DocumentViewBase
     {
-        public TradeServerManagerView(IViewContext viewContext, TradeServerManagerViewModel serverManagerViewModel)
-            : base(viewContext, serverManagerViewModel, ConfigurationModule.ModuleName)
+        public TradeServerManagerView(IViewContext viewContext, TradeServerManagerViewModel tradeServerManagerViewModel)
+            : base(viewContext, tradeServerManagerViewModel, ConfigurationModule.ModuleName)
         {
             InitializeComponent();
 
-            DataContext = serverManagerViewModel;
+            DataContext = tradeServerManagerViewModel;
         }
     }
 }
